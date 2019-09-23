35791f8dc799c2c7e37909b1e6dfdcb0d835bbf8
julia <julia@skylight.digital>
2019-09-23 08:08:26 -0400

    Adds sln; updates devcontainer config; more test work

---
+++ test/HedwigTests/TestClientProvider.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Hosting;
+
+namespace HedwigTests
+{
+	public class TestClientProvider : IDisposable
+	{
+		private TestServer _server;
+
+		public HttpClient Client { get; private set; }
+
+		public TestClientProvider()
+		{
+			_server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+
+			Client = _server.CreateClient();
+		}
+
+		public void Dispose()
+		{
+			_server?.Dispose();
+			Client?.Dispose();
+		}
+	}	
+}
