23abaa92a6d86d0e3d1c290580cadf8789dfbe8c
Melanie <melanie.mazanec@gmail.com>
2019-10-11 21:51:15 -0400

    Enable date range picker

--- src/Hedwig/ClientApp/src/components/RadioGroup/RadioGroup.tsx
+++ src/Hedwig/ClientApp/src/components/RadioGroup/RadioGroup.tsx
@@ -1,64 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 import RadioButton from './RadioButton';
 
 interface RadioButton {
-	text: string;
-	value: string;
-}
-
-type RadioGroupProps = {
-	options: RadioButton[];
-	groupName: string;
-  onClick: (value: string) => any;
-  legend: string;
-  selected?: string;
-	horizontal?: boolean;
-  showError?: boolean;
-  errorMessage?: string;
+  selected: boolean | undefined;
+  text: string,
+  value: string,
 };
 
-export default function RadioGroup({
-	options,
-	groupName,
-  onClick,
-  selected,
-	horizontal,
-  legend,
-  showError,
-  errorMessage,
-}: RadioGroupProps) {
+type RadioGroupProps = {
+  options: RadioButton[];
+  groupName: string,
+  onClick: (value: any) => any;
+  disabled?: boolean;
+  horizontal?: boolean;
+};
 
-  const [currentlySelected, setSelection] = useState(selected);
-  
-  function setRadioButtonSelection(value: string | undefined) {
-    setSelection(value);
-  }
-
-	return (
-		<fieldset className={`usa-fieldset${showError ? ' usa-form-group--error' : ''}`}>
-			<legend className="usa-sr-only">{legend}</legend>
-			{showError && errorMessage &&
-        <span className="usa-error-message" id="radio-group-error-message" role="alert">
-          {errorMessage}
-        </span>
-      }
-			<div className={horizontal ? 'grid-row flex-align-start grid-gap' : ''}>
-				{options.map(option => (
-					<RadioButton
-						value={option.value}
-						text={option.text}
-						onClick={event => {
-							const clicked = event.target.value;
-							setRadioButtonSelection(clicked);
-							onClick(clicked);
-						}}
-						key={option.value}
-						name={groupName}
-						selected={option.value === currentlySelected}
-						className={horizontal ? 'grid-col flex-auto' : ''}
-					/>
-				))}
-			</div>
-		</fieldset>
-	);
+export default function RadioGroup({ options, groupName, onClick, horizontal }: RadioGroupProps) {
+  return (
+    <div role="group" aria-describedby="" className={`${horizontal ? 'grid-row' : ''}`}>
+      {options.map(option =>
+        <RadioButton
+          value={option.value}
+          text={option.text}
+          onClick={value => onClick(value)}
+          key={option.value}
+          name={groupName}
+          selected={option.selected}
+        />)}
+    </div>
+  );
 }
