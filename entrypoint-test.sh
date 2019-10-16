c1540a7594d81341647219f7b2bd1c5da5834c77
julia <julia@skylight.digital>
2019-10-16 16:22:39 -0400

    Fix test run script to fail on build failure

--- entrypoint-test.sh
+++ entrypoint-test.sh
@@ -1,7 +1,11 @@
+set -e
 echo "waiting for database..."
 until dotnet ef -v --project src/Hedwig -v database update; do
     sleep 1
 done
 
+echo "building tests..."
+dotnet build test/HedwigTests
+
 echo "starting tests..."
-dotnet test test/HedwigTests --logger trx;LogFileName=Hedwig.trx
+dotnet test --no-build test/HedwigTests --logger trx;LogFileName=Hedwig.trx
