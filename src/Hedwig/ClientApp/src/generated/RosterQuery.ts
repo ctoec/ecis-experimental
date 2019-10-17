bd5262bbdea0c248e80929318cfa8d3124c7308c
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2019-10-17 13:34:26 +0000

    Add auth to graphql requests; use me query

--- src/Hedwig/ClientApp/src/generated/RosterQuery.ts
+++ src/Hedwig/ClientApp/src/generated/RosterQuery.ts
@@ -8,7 +8,7 @@ import { FundingSource } from "./globalTypes";
 // GraphQL query operation: RosterQuery
 // ====================================================
 
-export interface RosterQuery_user_sites_enrollments_child {
+export interface RosterQuery_me_sites_enrollments_child {
   __typename: "ChildType";
   firstName: string;
   middleName: string | null;
@@ -17,34 +17,34 @@ export interface RosterQuery_user_sites_enrollments_child {
   suffix: string | null;
 }
 
-export interface RosterQuery_user_sites_enrollments_fundings {
+export interface RosterQuery_me_sites_enrollments_fundings {
   __typename: "FundingType";
   entry: OECDate;
   exit: OECDate | null;
   source: FundingSource;
 }
 
-export interface RosterQuery_user_sites_enrollments {
+export interface RosterQuery_me_sites_enrollments {
   __typename: "EnrollmentType";
   id: number;
   entry: OECDate;
   exit: OECDate | null;
-  child: RosterQuery_user_sites_enrollments_child;
-  fundings: RosterQuery_user_sites_enrollments_fundings[];
+  child: RosterQuery_me_sites_enrollments_child;
+  fundings: RosterQuery_me_sites_enrollments_fundings[];
 }
 
-export interface RosterQuery_user_sites {
+export interface RosterQuery_me_sites {
   __typename: "SiteType";
   id: number;
   name: string;
-  enrollments: RosterQuery_user_sites_enrollments[];
+  enrollments: RosterQuery_me_sites_enrollments[];
 }
 
-export interface RosterQuery_user {
+export interface RosterQuery_me {
   __typename: "UserType";
-  sites: RosterQuery_user_sites[];
+  sites: RosterQuery_me_sites[];
 }
 
 export interface RosterQuery {
-  user: RosterQuery_user | null;
+  me: RosterQuery_me | null;
 }
