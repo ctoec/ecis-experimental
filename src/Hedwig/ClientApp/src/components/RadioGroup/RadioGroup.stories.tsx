8aed174a9ef349406de5732f18bfebee3b0fa71b
Melanie <melanie.mazanec@gmail.com>
2019-10-23 23:54:14 -0400

    Use prettier to make new components conform

--- src/Hedwig/ClientApp/src/components/RadioGroup/RadioGroup.stories.tsx
+++ src/Hedwig/ClientApp/src/components/RadioGroup/RadioGroup.stories.tsx
@@ -5,37 +5,37 @@ import RadioGroup from './RadioGroup';
 
 const onClick = action('onClick');
 const options = [
-  {
-    selected: true,
-    text: 'Option one',
-    value: 'one',
-  },
-  {
-    selected: false,
-    text: 'Option two',
-    value: 'two',
-  },
+	{
+		selected: true,
+		text: 'Option one',
+		value: 'one',
+	},
+	{
+		selected: false,
+		text: 'Option two',
+		value: 'two',
+	},
 ];
 
 storiesOf('RadioGroup', module)
-  .add('Default', () => {
-    return (
-      <RadioGroup
-        options={options}
-        groupName="storybook-radio-group"
-        onClick={onClick}
-        legend="Storybook radio buttons"
-      />
-    );
-  })
-  .add('Default', () => {
-    return (
-      <RadioGroup
-        options={options}
-        groupName="storybook-radio-group-2"
-        onClick={onClick}
-        legend="Storybook radio buttons"
-        horizontal={true}
-      />
-    );
-  });
+	.add('Default', () => {
+		return (
+			<RadioGroup
+				options={options}
+				groupName="storybook-radio-group"
+				onClick={onClick}
+				legend="Storybook radio buttons"
+			/>
+		);
+	})
+	.add('Horizontal', () => {
+		return (
+			<RadioGroup
+				options={options}
+				groupName="storybook-radio-group-2"
+				onClick={onClick}
+				legend="Storybook radio buttons"
+				horizontal={true}
+			/>
+		);
+	});
