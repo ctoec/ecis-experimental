a35d99ffa678c2cda412418c7f73c097b2b1e6b3
Melanie <melanie.mazanec@gmail.com>
2019-10-23 23:33:32 -0400

    Write stories

--- src/Hedwig/ClientApp/src/components/DatePicker/DatePicker.stories.tsx
+++ src/Hedwig/ClientApp/src/components/DatePicker/DatePicker.stories.tsx
@@ -1,1 +1,32 @@
-export default {}
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import moment from 'moment';
+import DatePicker from './DatePicker';
+
+const onSubmit = action('onSubmit');
+const onReset = action('onClick');
+const now = moment().local();
+const dateRange = { startDate: now, endDate: now };
+
+storiesOf('DatePicker', module)
+  .add('Default', () => {
+    return (
+      <DatePicker
+        onSubmit={onSubmit}
+        onReset={onReset}
+        dateRange={dateRange}
+        byRange={false}
+      />
+    );
+  })
+  .add('ByRange', () => {
+    return (
+      <DatePicker
+        onSubmit={onSubmit}
+        onReset={onReset}
+        dateRange={dateRange}
+        byRange={true}
+      />
+    );
+  })
