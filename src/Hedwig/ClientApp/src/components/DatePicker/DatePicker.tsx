2031108a29e522e8b9c4ba1fbeebce01de240b1a
Melanie <melanie.mazanec@gmail.com>
2019-10-24 19:02:01 -0400

    Move range logic into roster; make future out of date range

--- src/Hedwig/ClientApp/src/components/DatePicker/DatePicker.tsx
+++ src/Hedwig/ClientApp/src/components/DatePicker/DatePicker.tsx
@@ -6,12 +6,13 @@ import 'react-dates/lib/css/_datepicker.css';
 export type DateRange = {
 	startDate: Moment | null;
 	endDate: Moment | null;
-}
+};
 
 type DatePickerProps = {
-	onReset: (newRange: DateRange) => any;
+	onChange: (newRange: DateRange) => any;
 	dateRange: DateRange;
 	byRange: boolean;
+	possibleRange?: DateRange;
 };
 
 type DatePickerState = {
@@ -30,6 +31,7 @@ export class DatePicker extends React.Component<DatePickerProps, DatePickerState
 
 		this.resetState = this.resetState.bind(this);
 		this.setDateRange = this.setDateRange.bind(this);
+		this.isOutsidePossibleRange = this.isOutsidePossibleRange.bind(this);
 	}
 
 	resetState() {
@@ -40,12 +42,26 @@ export class DatePicker extends React.Component<DatePickerProps, DatePickerState
 				selectedRange: newRange,
 				datePickerFocused: null,
 			},
-			this.props.onReset(newRange)
+			this.props.onChange(newRange)
 		);
 	}
 
 	setDateRange(input: DateRange) {
-		this.setState({ selectedRange: input });
+		this.setState({ selectedRange: input }, this.props.onChange(input));
+	}
+
+	isOutsidePossibleRange(candidateDate: Moment) {
+		if (!this.props.possibleRange) {
+			return false;
+		}
+		const { startDate, endDate } = this.props.possibleRange;
+		if (startDate && candidateDate.isBefore(startDate)) {
+			return true;
+		}
+		if (endDate && candidateDate.isAfter(endDate)) {
+			return true;
+		}
+		return false;
 	}
 
 	render() {
@@ -53,33 +69,37 @@ export class DatePicker extends React.Component<DatePickerProps, DatePickerState
 		const { selectedRange, datePickerFocused } = this.state;
 
 		return (
-      <fieldset className="usa-fieldset">
-        <legend className="usa-sr-only">{`Choose a date${byRange ? ' range' : ''}`}</legend>
-        {byRange && (
-          <DateRangePicker
-            startDate={selectedRange.startDate}
-            startDateId="startDate"
-            endDate={selectedRange.endDate}
-            endDateId="endDate"
-            focusedInput={datePickerFocused}
-            onDatesChange={dates => this.setDateRange(dates)}
-            onFocusChange={(focused: any) => this.setState({ datePickerFocused: focused })}
-            isOutsideRange={() => false}
-          />
-        )}
-        {!byRange && (
-          <SingleDatePicker
-            id="date"
-            date={selectedRange.startDate}
-            focused={datePickerFocused === 'startDate'}
-            onDateChange={date => this.setDateRange({ startDate: date, endDate: date })}
-            onFocusChange={({ focused }: any) =>
-              this.setState({ datePickerFocused: focused ? 'startDate' : null })
-            }
-            isOutsideRange={() => false}
-          />
-        )}
-      </fieldset>
+			<fieldset className="usa-fieldset">
+				<legend className="usa-sr-only">{`Choose a date${byRange ? ' range' : ''}`}</legend>
+				{byRange && (
+					<DateRangePicker
+						startDate={selectedRange.startDate}
+						startDateId="startDate"
+						endDate={selectedRange.endDate}
+						endDateId="endDate"
+						focusedInput={datePickerFocused}
+						onDatesChange={dates => this.setDateRange(dates)}
+						onFocusChange={(focused: any) => this.setState({ datePickerFocused: focused })}
+						isOutsideRange={date => {
+							return this.isOutsidePossibleRange(date);
+						}}
+					/>
+				)}
+				{!byRange && (
+					<SingleDatePicker
+						id="date"
+						date={selectedRange.startDate}
+						focused={datePickerFocused === 'startDate'}
+						onDateChange={date => this.setDateRange({ startDate: date, endDate: date })}
+						onFocusChange={({ focused }: any) =>
+							this.setState({ datePickerFocused: focused ? 'startDate' : null })
+						}
+						isOutsideRange={date => {
+							return this.isOutsidePossibleRange(date);
+						}}
+					/>
+				)}
+			</fieldset>
 		);
 	}
 }
