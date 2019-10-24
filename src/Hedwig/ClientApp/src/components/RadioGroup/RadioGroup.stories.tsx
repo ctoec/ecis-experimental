efdf234fbea6b8e209f0a331fe5eebbbd1caff15
Melanie <melanie.mazanec@gmail.com>
2019-10-23 23:33:32 -0400

    Write stories

--- src/Hedwig/ClientApp/src/components/RadioGroup/RadioGroup.stories.tsx
+++ src/Hedwig/ClientApp/src/components/RadioGroup/RadioGroup.stories.tsx
@@ -1,28 +1,41 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { withKnobs, text } from '@storybook/addon-knobs';
-
 import RadioGroup from './RadioGroup';
 
-const defaultText = 'Click me';
 const onClick = action('onClick');
+const options = [
+  {
+    selected: true,
+    text: 'Option one',
+    value: 'one',
+  },
+  {
+    selected: false,
+    text: 'Option two',
+    value: 'two',
+  },
+];
 
 storiesOf('RadioGroup', module)
-  .addDecorator(withKnobs)
-  // .add('Default', () => {
-  //   const customText = text('Text', defaultText);
-  //   return <RadioGroup text={customText} onClick={onClick} />;
-  // })
-  // .add('Base', () => {
-  //   const customText = text('Text', defaultText);
-  //   return <RadioGroup text={customText} appearance="base" onClick={onClick} />;
-  // })
-  // .add('Secondary', () => {
-  //   const customText = text('Text', defaultText);
-  //   return <RadioGroup text={customText} appearance="secondary" onClick={onClick} />;
-  // })
-  // .add('Disabled', () => {
-  //   const customText = text('Text', defaultText);
-  //   return <RadioGroup text={customText} onClick={onClick} disabled />;
-  // });
+  .add('Default', () => {
+    return (
+      <RadioGroup
+        options={options}
+        groupName="storybook-radio-group"
+        onClick={onClick}
+        legend="Storybook radio buttons"
+      />
+    );
+  })
+  .add('Default', () => {
+    return (
+      <RadioGroup
+        options={options}
+        groupName="storybook-radio-group-2"
+        onClick={onClick}
+        legend="Storybook radio buttons"
+        horizontal={true}
+      />
+    );
+  });
