60117a70abab73b15c3b46421d8417fa7bddb814
julia <julia@skylight.digital>
2019-10-24 23:04:49 -0400

    96: Implement automatic token refresh

--- src/Hedwig/ClientApp/src/contexts/Login/LoginHOC.tsx
+++ src/Hedwig/ClientApp/src/contexts/Login/LoginHOC.tsx
@@ -7,7 +7,6 @@ import {
   TokenRequestHandler,
   AuthorizationServiceConfiguration,
   AuthorizationRequest,
-  AuthorizationResponse,
   TokenResponse,
   RedirectRequestHandler,
   BaseTokenRequestHandler,
@@ -51,63 +50,77 @@ const LoginProvider: React.FC<LoginProviderPropsType> = (
   const location = useLocation();
   const [firstLoad, setFirstLoad] = useState(true);
   const [accessToken, setAccessToken] = useState<string | null>(null);
+  const [persistentConfiguration, setPersistentConfiguration] = useState<AuthorizationServiceConfiguration>();
+  const [persistentTokenResponse, setPersistentTokenResponse] = useState<TokenResponse>();
 
   // state
   let configuration: AuthorizationServiceConfiguration | undefined;
-  let request: AuthorizationRequest | undefined;
-  let response: AuthorizationResponse | undefined;
-  let code: string | undefined;
-  let tokenResponse: TokenResponse | undefined;
 
-  // auth flow declaration
+  // auth flow 
   let notifier: AuthorizationNotifier;
   let authorizationHandler: AuthorizationRequestHandler;
   let tokenHandler: TokenRequestHandler;
-
-  // auth flow
-  notifier = new AuthorizationNotifier();
-  authorizationHandler = new RedirectRequestHandler(
-    // Use the default storage backend (i.e. local storage)
-    undefined,
-    // Identity Server is returning the authorization_code in the query string
-    // not URL hash. AppAuth hardcodes the use of hash in other logic.
-    // However, it does expose the ability to use query strings with the default
-    // BasicQueryStringUtils. By overriding the parse method, we can always
-    // require query string parsing irrespective of the supplied argument.
-    new (class extends BasicQueryStringUtils implements QueryStringUtils {
-      parse(input: LocationLike, _?: boolean): StringMap {
-        return super.parse(input, false);
-      }
-    })()
-  );
-  tokenHandler = new BaseTokenRequestHandler(new FetchRequestor());
-  // set notifier to deliver responses
-  authorizationHandler.setAuthorizationNotifier(notifier);
-  // set a listener to listen for authorization responses
-  notifier.setAuthorizationListener((req, resp, error) => {
-    if (resp) {
-      request = req;
-      response = resp;
-      code = resp.code;
-    }
-  });
-
-  // clean up
   let checkForAuthorizationResponseInterval: NodeJS.Timeout;
+  initAuthFlow();
 
-  function fetchServiceConfiguration(): Promise<void> {
-    if (configuration) {
-      return Promise.resolve();
+  /**
+   *  Instantiates openId/appauth flow control components
+   */
+  function initAuthFlow() {
+    tokenHandler = new BaseTokenRequestHandler(new FetchRequestor());
+
+    // set notifier to deliver authorization responses
+    notifier = new AuthorizationNotifier();
+    // set a listener to listen for and handle authorization responses
+    notifier.setAuthorizationListener((req, resp, error) => {
+      if (resp) {
+        let verifier: string | undefined;
+        if(req && req.internal) verifier = req.internal.code_verifier;
+        makeAuthorizationCodeTokenRequest(resp.code, verifier);
+      }
+    });
+    authorizationHandler = new RedirectRequestHandler(
+      // Use the default storage backend (i.e. local storage)
+      undefined,
+      // Identity Server is returning the authorization_code in the query string
+      // not URL hash. AppAuth hardcodes the use of hash in other logic.
+      // However, it does expose the ability to use query strings with the default
+      // BasicQueryStringUtils. By overriding the parse method, we can always
+      // require query string parsing irrespective of the supplied argument.
+      new (class extends BasicQueryStringUtils implements QueryStringUtils {
+        parse(input: LocationLike, _?: boolean): StringMap {
+          return super.parse(input, false);
+        }
+      })()
+    );
+    authorizationHandler.setAuthorizationNotifier(notifier);
+  }
+
+  /**
+   * Get service configuration from configured openId connect provider.
+   * This function should only need to be called once for the lifetime of the app
+   */
+  async function fetchServiceConfiguration(): Promise<void> {
+    if (persistentConfiguration) {
+      return;
     }
     return AuthorizationServiceConfiguration.fetchFromIssuer(openIdConnectUrl, new FetchRequestor())
       .then(resp => {
+        if(resp) {
         configuration = resp;
+        setPersistentConfiguration(resp);
+        }
       })
       .catch(error => {
         console.error(error);
       });
   }
 
+  /**
+   * Create and perform the initial authorization request.
+   * Redirects user to IdentityServer to grant permission,
+   * then returns to app to finish auth
+   */
   function makeAuthorizationRequest() {
     // create a request
     let request = new AuthorizationRequest({
@@ -124,74 +137,63 @@ const LoginProvider: React.FC<LoginProviderPropsType> = (
     }
   }
 
-  function makeTokenRequest() {
-    if (!configuration) {
-      return;
+  /**
+   * Create and perform authorization code-based token request.
+   * This type of token request is only required after initial auth request.
+   * Once a token is retrieve, the refresh token can be used to request subsquent
+   * tokens (until the refresh token expires)
+   */
+  async function makeAuthorizationCodeTokenRequest(code: string, verifier: string | undefined) {
+    if (!configuration) return;
+
+    let extras:  StringMap | undefined = undefined;
+    if(verifier) {
+      extras = { 'code_verifier': verifier };
     }
 
-    let req: TokenRequest | null = null;
-    if (code) {
-      let extras: StringMap | undefined = undefined;
-      if (request && request.internal) {
-        extras = {};
-        extras['code_verifier'] = request.internal['code_verifier'];
-      }
-      // use the code to make the token request.
-      req = new TokenRequest({
-        client_id: clientId,
-        redirect_uri: redirectUrl,
-        grant_type: GRANT_TYPE_AUTHORIZATION_CODE,
-        code: code,
-        refresh_token: undefined,
-        extras: extras
+    let req = new TokenRequest({
+      client_id: clientId,
+      redirect_uri: redirectUrl,
+      grant_type: GRANT_TYPE_AUTHORIZATION_CODE,
+      code: code,
+      refresh_token: undefined,
+      extras: extras
+    });
+    return tokenHandler.performTokenRequest(configuration, req)
+      .then((resp) => {
+        setAccessToken(resp.accessToken);
+        setPersistentTokenResponse(resp);
+        history.push('/');
       });
-    } else if (tokenResponse) {
-      // use the token response to make a request for an access token
-      req = new TokenRequest({
-        client_id: clientId,
-        redirect_uri: redirectUrl,
-        grant_type: GRANT_TYPE_REFRESH_TOKEN,
-        code: undefined,
-        refresh_token: tokenResponse.refreshToken,
-        extras: undefined
-      });
-    }
-
-    if (req) {
-      tokenHandler.performTokenRequest(configuration, req)
-        .then(resp => {
-          let isFirstRequest = false;
-          if (tokenResponse) {
-            // copy over new fields
-            tokenResponse.accessToken = resp.accessToken;
-            tokenResponse.issuedAt = resp.issuedAt;
-            tokenResponse.expiresIn = resp.expiresIn;
-            tokenResponse.tokenType = resp.tokenType;
-            tokenResponse.scope = resp.scope;
-          } else {
-            isFirstRequest = true;
-            tokenResponse = resp;
-          }
-
-          // unset code, so we can do refresh token exchanges subsequently
-          code = undefined;
-          if (isFirstRequest) {
-            setAccessToken(resp.accessToken);
-          } else {
-            // refresh token section
-          }
-          history.push('/');
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    }
   }
 
-  function checkForAuthorizationResponse() {
-    authorizationHandler.completeAuthorizationRequestIfPossible();
+  /**
+   * Create and perform token refresh request.
+   * Requires that initial code-based token request has already been performed.
+   */
+  async function makeRefreshTokenRequest() {
+    if (!persistentConfiguration) return;
+    if (!(persistentTokenResponse && persistentTokenResponse.refreshToken)) return;
+    // isValid includes a defaut 10 min expiration buffer.
+    if (persistentTokenResponse.isValid()) return;
+
+    let req = new TokenRequest({
+      client_id: clientId,
+      redirect_uri: redirectUrl,
+      grant_type: GRANT_TYPE_REFRESH_TOKEN,
+      code: undefined,
+      refresh_token: persistentTokenResponse.refreshToken,
+    });
+    tokenHandler.performTokenRequest(persistentConfiguration, req)
+      .then((resp) => {
+        setPersistentTokenResponse(resp);
+        setAccessToken(resp.accessToken); 
+      })
   }
 
+  /**
+   * Initiates login process from app
+   */
   const beginLogin = function() {
     if (firstLoad) {
       setFirstLoad(false);
@@ -199,20 +201,25 @@ const LoginProvider: React.FC<LoginProviderPropsType> = (
         .then(() => makeAuthorizationRequest());
       // the AppAuth library does not expose a promise for completion of the authorization request
       // so poll with second intervals to check for the response to complete request
-      checkForAuthorizationResponseInterval = setInterval(checkForAuthorizationResponse, 1000);
+      checkForAuthorizationResponseInterval = setInterval(authorizationHandler.completeAuthorizationRequestIfPossible, 1000);
     }
   }
 
+  /**
+   * Handles login completion upon redirect back to app from IdentityServer
+   */
   const callbackLogin = function() {
     if (firstLoad) {
       setFirstLoad(false);
       clearInterval(checkForAuthorizationResponseInterval);
       fetchServiceConfiguration()
         .then(() => authorizationHandler.completeAuthorizationRequestIfPossible())
-        .then(() => makeTokenRequest());
     }
   }
 
+  /**
+   * Triggers login actions based on location
+   */
   useEffect(() => {
     // only process requests that have the loginUriPrefix
     if (location.pathname === loginUriPrefix) {
@@ -222,10 +229,13 @@ const LoginProvider: React.FC<LoginProviderPropsType> = (
     }
   });
 
+  /**
+   * The wrapped LoginContext provider with instantiated values
+   */
   return (
     <InternalLoginProvider value={{
       accessToken: accessToken,
-      setAccessToken: setAccessToken
+      withFreshToken: makeRefreshTokenRequest
     }}>
       {children}
     </InternalLoginProvider>
@@ -233,14 +243,19 @@ const LoginProvider: React.FC<LoginProviderPropsType> = (
 }
 
 export type WithLoginPropsType = {
-  accessToken: string
+  accessToken: string,
+  withFreshToken: () => {}
 }
 
+/**
+ *  Wrapper function to turn arbitrary component into login consumer
+ * @param Component 
+ */
 const withLogin = <P extends WithLoginPropsType>(Component: React.FC<P>): React.FC<Subtract<P, WithLoginPropsType>> => 
   (props) => 
       <LoginConsumer>
         {({accessToken}) =>
-          <Component {...props as P} accessToken={accessToken} />
+          <Component {...props as P} accessToken={accessToken}/>
         }
       </LoginConsumer>
 
--- src/Hedwig/ClientApp/src/hooks/useAuthQuery.ts
+++ src/Hedwig/ClientApp/src/hooks/useAuthQuery.ts
@@ -1,17 +1,21 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { useQuery, QueryHookOptions } from '@apollo/react-hooks';
 import { DocumentNode } from 'graphql';
 import LoginContext from '../contexts/Login/LoginContext';
 import { QueryResult } from '@apollo/react-common';
 
 export default <T>(query: DocumentNode, options: QueryHookOptions = {}): QueryResult<T> => {
-  const { accessToken } = useContext(LoginContext);
+  const { accessToken, withFreshToken } = useContext(LoginContext);
 
+  useEffect(() => {
+    withFreshToken();
+  });
+    
   return useQuery<T>(query, {
     ...options,
     context: {
       headers: {
-        Authorization: accessToken ? `Bearer ${accessToken}` : ''
+        authorization: accessToken ? `Bearer ${accessToken}` : ''
       }
     },
     fetchPolicy: 'cache-and-network'
