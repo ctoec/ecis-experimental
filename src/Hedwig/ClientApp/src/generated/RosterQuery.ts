ae5b8ab2028032558d743cd4e34da39a0faef5d8
Chris Given <cmgiven@gmail.com>
2019-10-29 09:49:38 -0500

    Update apollo generate

--- src/Hedwig/ClientApp/src/generated/RosterQuery.ts
+++ src/Hedwig/ClientApp/src/generated/RosterQuery.ts
@@ -2,49 +2,50 @@
 /* eslint-disable */
 // This file was automatically generated and should not be edited.
 
-import { FundingSource } from "./globalTypes";
+import { FundingSource } from './globalTypes';
 
 // ====================================================
 // GraphQL query operation: RosterQuery
 // ====================================================
 
 export interface RosterQuery_me_sites_enrollments_child {
-  __typename: "ChildType";
-  firstName: string;
-  middleName: string | null;
-  lastName: string;
-  birthdate: OECDate;
-  suffix: string | null;
+	__typename: 'ChildType';
+	id: string;
+	firstName: string;
+	middleName: string | null;
+	lastName: string;
+	birthdate: OECDate;
+	suffix: string | null;
 }
 
 export interface RosterQuery_me_sites_enrollments_fundings {
-  __typename: "FundingType";
-  entry: OECDate;
-  exit: OECDate | null;
-  source: FundingSource;
+	__typename: 'FundingType';
+	entry: OECDate;
+	exit: OECDate | null;
+	source: FundingSource;
 }
 
 export interface RosterQuery_me_sites_enrollments {
-  __typename: "EnrollmentType";
-  id: number;
-  entry: OECDate;
-  exit: OECDate | null;
-  child: RosterQuery_me_sites_enrollments_child;
-  fundings: RosterQuery_me_sites_enrollments_fundings[];
+	__typename: 'EnrollmentType';
+	id: number;
+	entry: OECDate;
+	exit: OECDate | null;
+	child: RosterQuery_me_sites_enrollments_child;
+	fundings: RosterQuery_me_sites_enrollments_fundings[];
 }
 
 export interface RosterQuery_me_sites {
-  __typename: "SiteType";
-  id: number;
-  name: string;
-  enrollments: RosterQuery_me_sites_enrollments[];
+	__typename: 'SiteType';
+	id: number;
+	name: string;
+	enrollments: RosterQuery_me_sites_enrollments[];
 }
 
 export interface RosterQuery_me {
-  __typename: "UserType";
-  sites: RosterQuery_me_sites[];
+	__typename: 'UserType';
+	sites: RosterQuery_me_sites[];
 }
 
 export interface RosterQuery {
-  me: RosterQuery_me | null;
+	me: RosterQuery_me | null;
 }
