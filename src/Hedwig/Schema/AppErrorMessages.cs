61efeb675ea6531bb431c8c9f2d7621669529faf
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2019-11-04 15:27:35 -0500

    Update child model (PR 198) (Issue 187)
    Add birth location, sasid, foster, nullable birth date and gender; remove nullable bool

--- src/Hedwig/Schema/AppErrorMessages.cs
+++ src/Hedwig/Schema/AppErrorMessages.cs
@@ -3,10 +3,12 @@ namespace Hedwig.Schema
     public static class AppErrorMessages
     {
         private static string NOT_FOUND_TMPL = "{0} with id {1} not found";
-        public static string NOT_FOUND(string type, int id) {
+
+        public static string NOT_FOUND(string type, string id) {
             return string.Format(NOT_FOUND_TMPL, type, id);
         }
-        public static string NOT_FOUND(string type, string id) {
+
+        public static string NOT_FOUND(string type, int id) {
             return string.Format(NOT_FOUND_TMPL, type, id);
         }
     }
