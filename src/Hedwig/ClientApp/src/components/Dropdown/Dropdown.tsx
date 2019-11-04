ca5f1426864c5702bde05cacd432e8d9623ef593
Melanie Mazanec <12260067+mmazanec22@users.noreply.github.com>
2019-11-04 11:05:40 -0500

    Create dropdown component (#131) (#201)
    * Add dropdown component
    
    * Bump USWDS version to resolve hero image path issues

---
+++ src/Hedwig/ClientApp/src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+type DropdownOption = {
+	value: string;
+	text: string;
+};
+
+type DropdownProps = {
+	options: DropdownOption[];
+	label: string;
+	unselectedOptionText: string;
+	onChange: () => any;
+	id?: string;
+	success?: boolean;
+	disabled?: boolean;
+	error?: boolean;
+	errorMessage?: string;
+};
+
+export default function Dropdown({
+	options,
+	label,
+	unselectedOptionText,
+	onChange,
+	id,
+	success,
+	disabled,
+	error,
+	errorMessage,
+}: DropdownProps) {
+	const selectId = id || label.split(' ').join('-');
+	const errorMessageId = `${selectId}-error-message`;
+
+	return (
+		<div className={`usa-form-group${error ? ' usa-form-group--error' : ''}`}>
+			<label className={`usa-label${error ? ' usa-label--error' : ''}`} htmlFor={selectId}>
+				{label}
+			</label>
+			{error && errorMessage && (
+				<span className="usa-error-message" id={errorMessageId} role="alert">
+					{errorMessage}
+				</span>
+			)}
+			<select
+				className={`usa-select${error ? ' usa-input--error' : ''}${
+					success ? ' usa-input--success' : ''
+				}`}
+				name={selectId}
+				id={selectId}
+				onChange={onChange}
+				disabled={disabled}
+				aria-describedby={error ? errorMessageId : undefined}
+			>
+				<option value={undefined}>{unselectedOptionText}</option>
+				{options.map((option, i) => (
+					<option value={option.value} key={`${option.value}-${i}`}>
+						{option.text}
+					</option>
+				))}
+			</select>
+		</div>
+	);
+}
