535011179e34d9f8682eff74de8b1d3c398a5f6b
Melanie <melanie.mazanec@gmail.com>
2019-11-04 17:16:27 -0500

    Create alert component

---
+++ src/Hedwig/ClientApp/src/components/Alert/Alert.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+type AlertProps = {
+	type: 'success' | 'warning' | 'error' | 'info';
+	heading: string;
+	text: string;
+};
+
+export default function Alert({ type, heading, text }: AlertProps) {
+	return (
+		<div className={`usa-alert usa-alert--${type}`} role={type === 'error' ? 'alert' : undefined}>
+			<div className="usa-alert__body">
+				<h3 className="usa-alert__heading">{heading}</h3>
+				<p className="usa-alert__text">{text}</p>
+			</div>
+		</div>
+	);
+}
