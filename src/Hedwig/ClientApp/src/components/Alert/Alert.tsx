8fc000ea60ee50220932bf8408ac79cbc10376e5
Melanie <melanie.mazanec@gmail.com>
2019-11-05 09:55:26 -0500

    Use h2 for alerts to avoid screen reader confusion

--- src/Hedwig/ClientApp/src/components/Alert/Alert.tsx
+++ src/Hedwig/ClientApp/src/components/Alert/Alert.tsx
@@ -11,7 +11,7 @@ export default function Alert({ type, heading, text }: AlertProps) {
 	return (
 		<div className={`usa-alert usa-alert--${type}`} role={type === 'error' ? 'alert' : undefined}>
 			<div className="usa-alert__body">
-				<h3 className="usa-alert__heading">{heading}</h3>
+				<h2 className="usa-alert__heading">{heading}</h2>
 				<p className="usa-alert__text">{text}</p>
 			</div>
 		</div>
