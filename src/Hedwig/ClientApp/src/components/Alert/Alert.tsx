e41502bbddc0468f8bf10fd9c02474e0a0098af2
Melanie <melanie.mazanec@gmail.com>
2019-11-05 08:57:02 -0500

    Note aria role

--- src/Hedwig/ClientApp/src/components/Alert/Alert.tsx
+++ src/Hedwig/ClientApp/src/components/Alert/Alert.tsx
@@ -7,6 +7,7 @@ type AlertProps = {
 };
 
 export default function Alert({ type, heading, text }: AlertProps) {
+	// If in the future we make this interactive, we should use the alertdialog role
 	return (
 		<div className={`usa-alert usa-alert--${type}`} role={type === 'error' ? 'alert' : undefined}>
 			<div className="usa-alert__body">
