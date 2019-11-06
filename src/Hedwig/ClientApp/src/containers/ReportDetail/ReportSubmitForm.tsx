d9792ecb09649a09dca2bde0821ab631a81e274f
Melanie Mazanec <12260067+mmazanec22@users.noreply.github.com>
2019-11-06 13:19:17 -0500

    Add success to report (#220)
    * Start to add alert to submit form
    
    * Move report to outside of submit form
    
    * Update snapshots

--- src/Hedwig/ClientApp/src/containers/ReportDetail/ReportSubmitForm.tsx
+++ src/Hedwig/ClientApp/src/containers/ReportDetail/ReportSubmitForm.tsx
@@ -3,6 +3,7 @@ import useAuthMutation from '../../hooks/useAuthMutation';
 import { gql } from 'apollo-boost';
 import { ReportQuery_report } from '../../generated/ReportQuery';
 import { ReportSubmitMutation } from '../../generated/ReportSubmitMutation';
+import Alert, { AlertProps } from '../../components/Alert/Alert';
 
 export const REPORT_SUBMIT_MUTATION = gql`
 	mutation ReportSubmitMutation($id: Int!, $accredited: Boolean!) {
@@ -18,13 +19,20 @@ export const REPORT_SUBMIT_MUTATION = gql`
 export default function ReportSubmitForm(report: ReportQuery_report) {
 	const [accredited, setAccredited] = useState(report.accredited);
 	const [submittedAt, setSubmittedAt] = useState(report.submittedAt);
+	const [alert, setAlert] = useState<AlertProps | null>(null);
 
 	const [updateReport] = useAuthMutation<ReportSubmitMutation>(REPORT_SUBMIT_MUTATION, {
 		onCompleted: data => {
 			setSubmittedAt(data && data.submitCdcReport ? data.submitCdcReport.submittedAt : null);
+			setAlert({
+				type: 'success',
+				heading: 'Report submitted',
+				text: 'You have successfully submitted this report.',
+			});
 		},
 		onError: error => {
 			console.log(error);
+			setAlert({ type: 'error', heading: 'Report not submitted', text: error.message });
 		},
 	});
 
@@ -34,26 +42,29 @@ export default function ReportSubmitForm(report: ReportQuery_report) {
 	}
 
 	return (
-		<form className="usa-form" onSubmit={onSubmit}>
-			{submittedAt && (
-				<p>
-					<b>Submitted At:</b> {submittedAt}{' '}
-				</p>
-			)}
-			<div className="usa-checkbox">
-				<input
-					className="usa-checkbox__input"
-					id="accredited"
-					type="checkbox"
-					disabled={!!submittedAt}
-					defaultChecked={accredited}
-					onChange={e => setAccredited(e.target.checked)}
-				/>
-				<label className="usa-checkbox__label" htmlFor="accredited">
-					Accredited
-				</label>
-			</div>
-			{!submittedAt && <input className="usa-button" type="submit" value="Submit" />}
-		</form>
+		<React.Fragment>
+			{alert && <Alert {...alert} />}
+			<form className="usa-form" onSubmit={onSubmit}>
+				{submittedAt && (
+					<p>
+						<b>Submitted At:</b> {submittedAt}{' '}
+					</p>
+				)}
+				<div className="usa-checkbox">
+					<input
+						className="usa-checkbox__input"
+						id="accredited"
+						type="checkbox"
+						disabled={!!submittedAt}
+						defaultChecked={accredited}
+						onChange={e => setAccredited(e.target.checked)}
+					/>
+					<label className="usa-checkbox__label" htmlFor="accredited">
+						Accredited
+					</label>
+				</div>
+				{!submittedAt && <input className="usa-button" type="submit" value="Submit" />}
+			</form>
+		</React.Fragment>
 	);
 }
