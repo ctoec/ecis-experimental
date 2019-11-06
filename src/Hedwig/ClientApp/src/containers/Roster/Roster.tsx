e72150c3bd666908fe505a94c75a9d7e83696b69
jlhogan <julia@skylight.digital>
2019-11-06 12:23:12 -0500

    color styles for funding tags in roster

--- src/Hedwig/ClientApp/src/containers/Roster/Roster.tsx
+++ src/Hedwig/ClientApp/src/containers/Roster/Roster.tsx
@@ -97,16 +97,7 @@ export default function Roster() {
 			{
 				name: 'Funding',
 				cell: ({ row }) => (
-					<td>
-						{row.fundings.length ? (
-							<Tag
-								text={`${row.fundings[0].source}`}
-								color={getColorForFundingSource(row.fundings[0].source)}
-							/>
-						) : (
-							''
-						)}
-					</td>
+					<td>{row.fundings.length ? <Tag text={`${row.fundings[0].source}`} color={getColorForFundingSource(row.fundings[0].source)}/> : ''}</td>
 				),
 			},
 			{
