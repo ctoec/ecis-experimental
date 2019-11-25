82a3c117ff04a65895c195022bffd55e4cc9b494
julia <julia@skylight.digital>
2019-11-25 14:29:15 -0500

    134: Validation POC

--- src/Hedwig/Models/Enrollment.cs
+++ src/Hedwig/Models/Enrollment.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Hedwig.Models
 {
-	public class Enrollment : TemporalEntity
+	public class Enrollment : TemporalEntity, IValidatableObject
 	{
 		public int Id { get; set; }
 
@@ -20,5 +21,16 @@ namespace Hedwig.Models
 		public DateTime? Exit { get; set; }
 
 		public ICollection<Funding> Fundings { get; set; }
+
+		public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		{
+			if (Entry.HasValue && Exit.HasValue && Entry.Value.CompareTo(Exit.Value) >= 0)
+			{
+				yield return new ValidationResult(
+					$"Exit cannot be before Entry",
+					new [] {nameof(Exit)}
+				);
+			}
+		}
 	}
 }
