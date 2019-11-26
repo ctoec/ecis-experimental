304199b6f259ce4020a6255f5f37ba0da8084247
Chris Given <cmgiven@gmail.com>
2019-11-26 16:11:32 -0600

    FundingTime too

--- src/Hedwig/Models/Funding.cs
+++ src/Hedwig/Models/Funding.cs
@@ -12,6 +12,8 @@ namespace Hedwig.Models
 
 		[JsonConverter(typeof(StringEnumConverter))]
 		public FundingSource Source { get; set; }
+
+		[JsonConverter(typeof(StringEnumConverter))]
 		public FundingTime Time { get; set; }
 	}
 }
