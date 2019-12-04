b1da0c7b1c6327ea147e3d2421e1d79ab86215dd
Melanie Mazanec <12260067+mmazanec22@users.noreply.github.com>
2019-12-04 15:54:49 -0500

    Use enrollment (#341)
    * Use enrollment id for param
    
    * Format with tabs not spaces

--- src/Hedwig/ClientApp/src/containers/Roster/Roster.tsx
+++ src/Hedwig/ClientApp/src/containers/Roster/Roster.tsx
@@ -30,25 +30,21 @@ export default function Roster() {
 				id: idx(user, _ => _.orgPermissions[0].organization.sites[0].id) || 0,
 			}),
 		[user]
-  );
-  
-  	const [enrollmentsLoading, enrollmentsError, rawEnrollments] = useApi(
-			// TODO: after everything being nullable is solved, ditch raw enrollments and type mapping below
-			api =>
-				api.apiOrganizationsOrgIdSitesSiteIdEnrollmentsGet({
-					// TODO after pilot: don't just grab the first org and site
-					orgId: idx(user, _ => _.orgPermissions[0].organization.id) || 0,
-					siteId: idx(user, _ => _.orgPermissions[0].organization.sites[0].id) || 0,
-					include: ['child', 'fundings'],
-					startDate:
-						(dateRange && dateRange.startDate && dateRange.startDate.toDate()) ||
-						undefined,
-					endDate:
-						(dateRange && dateRange.endDate && dateRange.endDate.toDate()) ||
-						undefined,
-				}),
-			[user, dateRange]
-		);
+	);
+
+	const [enrollmentsLoading, enrollmentsError, rawEnrollments] = useApi(
+		// TODO: after everything being nullable is solved, ditch raw enrollments and type mapping below
+		api =>
+			api.apiOrganizationsOrgIdSitesSiteIdEnrollmentsGet({
+				// TODO after pilot: don't just grab the first org and site
+				orgId: idx(user, _ => _.orgPermissions[0].organization.id) || 0,
+				siteId: idx(user, _ => _.orgPermissions[0].organization.sites[0].id) || 0,
+				include: ['child', 'fundings'],
+				startDate: (dateRange && dateRange.startDate && dateRange.startDate.toDate()) || undefined,
+				endDate: (dateRange && dateRange.endDate && dateRange.endDate.toDate()) || undefined,
+			}),
+		[user, dateRange]
+	);
 
 	function handlePastEnrollmentsChange() {
 		toggleShowPastEnrollments(!showPastEnrollments);
@@ -58,8 +54,8 @@ export default function Roster() {
 
 	if (!site) {
 		return <div className="Roster"></div>;
-  }
-  
+	}
+
 	// TODO: FIX THIS-- ditch raw enrollments
 	const enrollments = (rawEnrollments || []).map(e => e as Required<Enrollment>);
 
@@ -72,7 +68,7 @@ export default function Roster() {
 				name: 'Name',
 				cell: ({ row }) => (
 					<th scope="row">
-						<Link to={`/roster/enrollments/${row.child.id}/`} className="usa-link">
+						<Link to={`/roster/enrollments/${row.id}/`} className="usa-link">
 							{nameFormatter(row.child)}
 						</Link>
 					</th>
