06f8ff10412968b4de004174d5ac7a1f580bc610
Melanie Mazanec <12260067+mmazanec22@users.noreply.github.com>
2019-12-05 13:11:31 -0500

    Add draft tag to unsubmitted reports (#347)
    * Add draft tag to unsubmitted reports

--- src/Hedwig/ClientApp/src/components/Tag/Tag.tsx
+++ src/Hedwig/ClientApp/src/components/Tag/Tag.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
 export type TagProps = {
-    text: String;
-    color?: string;
+	text: String;
+	color?: string;
+	addClass?: string;
 };
 
-export default function Tag({ text, color}: TagProps) {
-    var className = "usa-tag";
-    if (color) className += ` bg-${color}`;
-    return (
-        <span className={className}>{text}</span>
-    );
+export default function Tag({ text, color, addClass }: TagProps) {
+	var className = `usa-tag ${addClass}`;
+	if (color) className += ` bg-${color}`;
+	return <span className={className}>{text}</span>;
 }
