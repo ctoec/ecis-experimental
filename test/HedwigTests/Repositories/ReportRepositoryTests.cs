9579faebff91cba0e61bfb41df8a74d16395342f
Chris Given <cmgiven@gmail.com>
2019-12-05 12:29:48 -0600

    Fix a bad merge, and remove a warning

--- test/HedwigTests/Repositories/ReportRepositoryTests.cs
+++ test/HedwigTests/Repositories/ReportRepositoryTests.cs
@@ -12,34 +12,6 @@ namespace HedwigTests.Repositories
 {
   public class ReportRepositoryTests
   {
-    [Fact]
-    public async Task Get_Reports_For_User_With_Organization_Permission()
-    {
-      using (var context = new TestContextProvider().Context)
-      {
-        // If organization permissions exist with user Ids, and each has an associated report
-        var user = UserHelper.CreateUser(context);
-        var orgPermission1 = PermissionHelper.CreateOrganizationPermission(context, user: user);
-        var orgPermission2 = PermissionHelper.CreateOrganizationPermission(context, user: user);
-        var report1 = ReportHelper.CreateCdcReport(context, organization: orgPermission1.Organization);
-        var report2 = ReportHelper.CreateCdcReport(context, organization: orgPermission2.Organization);
-
-        var otherUser = UserHelper.CreateUser(context);
-        var otherOrganizationPermission = PermissionHelper.CreateOrganizationPermission(context, user: otherUser);
-        var otherReport = ReportHelper.CreateCdcReport(context, organization: otherOrganizationPermission.Organization);
-
-        // When the report repository is queried with a user id
-        var reportRepo = new ReportRepository(context);
-        var result = await reportRepo.GetReportsByUserIdAsync(user.Id);
-
-        // Then a list of reports for which that user has permission is returned
-        var reportIds = (from x in result select x.Id).ToArray();
-        Assert.Contains(report1.Id, reportIds);
-        Assert.Contains(report2.Id, reportIds);
-        Assert.DoesNotContain(otherReport.Id, reportIds);
-      }
-    }
-
     [Theory]
     [InlineData(false, new string[] { })]
     [InlineData(false, new string[] { "organizations" })]
@@ -106,47 +78,47 @@ namespace HedwigTests.Repositories
       }
     }
 
-		[Fact]
-		public async Task Get_Reports_For_User_With_Organization_Permission()
-		{
-			using (var context = new TestContextProvider().Context)
-			{
-				// If organization permissions exist with user Ids, and each has an associated report
-				var user = UserHelper.CreateUser(context);
-				var orgPermission1 = PermissionHelper.CreateOrganizationPermission(context, user: user);
-				var orgPermission2 = PermissionHelper.CreateOrganizationPermission(context, user: user);
-				var report1 = ReportHelper.CreateCdcReport(context, organization: orgPermission1.Organization);
-				var report2 = ReportHelper.CreateCdcReport(context, organization: orgPermission2.Organization);
+    [Fact]
+    public async Task Get_Reports_For_User_With_Organization_Permission()
+    {
+      using (var context = new TestContextProvider().Context)
+      {
+        // If organization permissions exist with user Ids, and each has an associated report
+        var user = UserHelper.CreateUser(context);
+        var orgPermission1 = PermissionHelper.CreateOrganizationPermission(context, user: user);
+        var orgPermission2 = PermissionHelper.CreateOrganizationPermission(context, user: user);
+        var report1 = ReportHelper.CreateCdcReport(context, organization: orgPermission1.Organization);
+        var report2 = ReportHelper.CreateCdcReport(context, organization: orgPermission2.Organization);
 
-				var otherUser = UserHelper.CreateUser(context);
-				var otherOrganizationPermission = PermissionHelper.CreateOrganizationPermission(context, user: otherUser);
-				var otherReport = ReportHelper.CreateCdcReport(context, organization: otherOrganizationPermission.Organization);
+        var otherUser = UserHelper.CreateUser(context);
+        var otherOrganizationPermission = PermissionHelper.CreateOrganizationPermission(context, user: otherUser);
+        var otherReport = ReportHelper.CreateCdcReport(context, organization: otherOrganizationPermission.Organization);
 
-				// When the report repository is queried with a user id
-				var reportRepo = new ReportRepository(context);
-				var result = await reportRepo.GetReportsByUserIdAsync(user.Id);
+        // When the report repository is queried with a user id
+        var reportRepo = new ReportRepository(context);
+        var result = await reportRepo.GetReportsByUserIdAsync(user.Id);
 
-				// Then a list of reports for which that user has permission is returned
-				var reportIds = (from x in result select x.Id).ToArray();
-				Assert.Contains(report1.Id, reportIds);
-				Assert.Contains(report2.Id, reportIds);
-				Assert.DoesNotContain(otherReport.Id, reportIds);
-			}
-		}
-		[Fact]
-		public async Task GetReportsForOrganization()
-		{
-			using (var context = new TestContextProvider().Context)
-			{
-				var organization = OrganizationHelper.CreateOrganization(context);
-				var reports = ReportHelper.CreateCdcReports(context, 5, organization: organization);
-				ReportHelper.CreateCdcReport(context);
+        // Then a list of reports for which that user has permission is returned
+        var reportIds = (from x in result select x.Id).ToArray();
+        Assert.Contains(report1.Id, reportIds);
+        Assert.Contains(report2.Id, reportIds);
+        Assert.DoesNotContain(otherReport.Id, reportIds);
+      }
+    }
+    [Fact]
+    public async Task GetReportsForOrganization()
+    {
+      using (var context = new TestContextProvider().Context)
+      {
+        var organization = OrganizationHelper.CreateOrganization(context);
+        var reports = ReportHelper.CreateCdcReports(context, 5, organization: organization);
+        ReportHelper.CreateCdcReport(context);
 
-				var reportRepo = new ReportRepository(context);
-				var result = await reportRepo.GetReportsForOrganization(organization.Id);
+        var reportRepo = new ReportRepository(context);
+        var result = await reportRepo.GetReportsForOrganization(organization.Id);
 
-				Assert.Equal(reports, result);
-			}
-		}
-	}
+        Assert.Equal(reports, result);
+      }
+    }
+  }
 }
