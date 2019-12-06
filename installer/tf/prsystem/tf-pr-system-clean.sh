0ed51a057d8ea7245544de3fb0e778c211883962
pdobrzanski <speedcm@myactv.net>
2019-12-06 10:05:34 -0500

    added api call for clean script

--- installer/tf/prsystem/tf-pr-system-clean.sh
+++ installer/tf/prsystem/tf-pr-system-clean.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$(realpath "$0")")"
 s3_workspaces_output=`aws s3 ls s3://ct-oec-hedwig-cicd-pr-terraform/env: --recursive | sed "s|/terraform.tfstate.*||" | sed "s|.*/||"`
 for workspace in $s3_workspaces_output
 do
-   if curl https://github.com/ctoec/ecis-experimental/pull/${workspace} | grep redirected 
+   if curl https://api.github.com/repos/ctoec/ecis-experimental/pulls/${workspace} | grep state | grep closed
    then
       echo deleting terraform system - $workspace
       ./tf-pr-system-destroy.sh ${workspace}
