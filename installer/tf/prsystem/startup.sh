b783704fc19593af3aa20a856041717034d52d40
pdobrzanski <speedcm@myactv.net>
2019-12-06 14:38:27 -0500

    add startup parameter changes

--- installer/tf/prsystem/startup.sh
+++ installer/tf/prsystem/startup.sh
@@ -59,9 +59,12 @@ sudo mount -o bind /home/ubuntu/ws/winged-keys /home/ubuntu/ws/ecis-experimental
 #---------------------------------------
 publicIP=$(curl https://checkip.amazonaws.com)
 currentHost=https://$publicIP:5001
-sudo -i -u ubuntu bash -c 'sed -i "s|https://localhost:5050|'"$currentHost"'/|g" /home/ubuntu/ws/ecis-experimental/src/Hedwig/ClientApp/public/config.json'
-sudo -i -u ubuntu bash -c 'sed -i "s|\(^\s*\"BaseUri\":\).*$|\1\"'"$currentHost"'\",|" /home/ubuntu/ws/winged-keys/src/WingedKeys/appsettings.Development.json'
-sudo -i -u ubuntu bash -c 'sed -i "s|\(^\s*\"ClientUris\":\).*$|\1\"'"$currentHost"'\",|" /home/ubuntu/ws/winged-keys/src/WingedKeys/appsettings.Development.json'
+#sudo -i -u ubuntu bash -c 'sed -i "s|https://localhost:5050|'"$currentHost"'/|g" /home/ubuntu/ws/ecis-experimental/src/Hedwig/ClientApp/public/config.json'
+#sudo -i -u ubuntu bash -c 'sed -i "s|\(^\s*\"BaseUri\":\).*$|\1\"'"$currentHost"'\",|" /home/ubuntu/ws/winged-keys/src/WingedKeys/appsettings.Development.json'
+#sudo -i -u ubuntu bash -c 'sed -i "s|\(^\s*\"ClientUris\":\).*$|\1\"'"$currentHost"'\",|" /home/ubuntu/ws/winged-keys/src/WingedKeys/appsettings.Development.json'
+
+sudo -i -u ubuntu bash -c 'sed -i "s|localhost|'"$publicIP"'|g" /home/ubuntu/ws/ecis-experimental/src/Hedwig/ClientApp/public/config.json'
+sudo -i -u ubuntu bash -c 'sed -i "s|localhost|'"$publicIP"'|g" /home/ubuntu/ws/winged-keys/src/WingedKeys/appsettings.Development.json'
 
 #---------------------------------------
 # Startup docker
