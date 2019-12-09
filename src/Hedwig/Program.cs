b158b60ed68f83f35ffce9923de65cccb605dc00
Chris Given <cmgiven@gmail.com>
2019-12-09 17:59:31 -0600

    Better seeding of data

--- src/Hedwig/Program.cs
+++ src/Hedwig/Program.cs
@@ -7,53 +7,54 @@ using Hedwig.Data;
 
 namespace Hedwig
 {
-	public class Program
-	{
-		protected Program() {}
+  public class Program
+  {
+    protected Program() { }
 
-		public static void Main(string[] args)
-		{
-			var host = CreateHostBuilder(args).Build();
+    public static void Main(string[] args)
+    {
+      var host = CreateHostBuilder(args).Build();
 
-			using (var scope = host.Services.CreateScope())
-			{
-				var services = scope.ServiceProvider;
-				try
-				{
-					var context = services.GetRequiredService<HedwigContext>();
-					DbInitializer.Initialize(context);
-				}
-				catch (Exception ex)
-				{
-					var logger = services.GetRequiredService<ILogger<Program>>();
-					logger.LogError(ex, "An error occurred while seeding the database.");
-				}
-			}
+      using (var scope = host.Services.CreateScope())
+      {
+        var services = scope.ServiceProvider;
+        try
+        {
+          var context = services.GetRequiredService<HedwigContext>();
+          var initializer = new DbInitializer(context);
+          initializer.Initialize();
+        }
+        catch (Exception ex)
+        {
+          var logger = services.GetRequiredService<ILogger<Program>>();
+          logger.LogError(ex, "An error occurred while seeding the database.");
+        }
+      }
 
-			host.Run();
-		}
+      host.Run();
+    }
 
-		public static IHostBuilder CreateHostBuilder(string[] args) =>
-			Host.CreateDefaultBuilder(args)
-				.ConfigureLogging((context, logging) =>
-				{
-				  logging.ClearProviders();
-				  logging.AddConfiguration(context.Configuration.GetSection("Logging"));
-				  logging.AddConsole();
-				  logging.AddDebug();
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+      Host.CreateDefaultBuilder(args)
+        .ConfigureLogging((context, logging) =>
+        {
+          logging.ClearProviders();
+          logging.AddConfiguration(context.Configuration.GetSection("Logging"));
+          logging.AddConsole();
+          logging.AddDebug();
 
-					var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-					var isDevelopment = environment == Microsoft.Extensions.Hosting.Environments.Development;
-				  if (!isDevelopment)
-				  {
-  					logging.AddAWSProvider();
-				  }
+          var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+          var isDevelopment = environment == Microsoft.Extensions.Hosting.Environments.Development;
+          if (!isDevelopment)
+          {
+            logging.AddAWSProvider();
+          }
 
-				})
-				.ConfigureWebHostDefaults(webBuilder =>
-				{
-					webBuilder.UseStartup<Startup>();
-				});
-	}
+        })
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+          webBuilder.UseStartup<Startup>();
+        });
+  }
 
 }
