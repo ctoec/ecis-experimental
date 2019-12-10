44ecd109495a6e1ec6cc10abf82bc6ac9052af06
Chris Given <cmgiven@gmail.com>
2019-12-10 10:45:36 -0600

    Reports list is organzed by pending and submitted reports (#385)
    * Updates the nav bar to use a count of unsubmitted reports, per designs
    * Refreshes the nav bar after report submit
    * Removes unnecessary state from ReportSubmitForm (useApi takes care of it)
    * Unifies currency formatting and text input validations, closing out some todos left as comments

--- src/Hedwig/ClientApp/src/containers/App/App.tsx
+++ src/Hedwig/ClientApp/src/containers/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, createContext } from 'react';
 import { Switch } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import { NavItemProps } from '../../components/Header/NavItem';
@@ -10,57 +10,66 @@ import { ApiOrganizationsOrgIdReportsGetRequest } from '../../OAS-generated';
 import getIdForUser from '../../utils/getIdForUser';
 import UserContext from '../../contexts/User/UserContext';
 
+type AppContextType = { invalidateCache: () => void };
+export const AppContext = createContext<AppContextType>({ invalidateCache: () => { } });
+
 const App: React.FC = () => {
-	const { user } = useContext(UserContext);
-	
-	const params: ApiOrganizationsOrgIdReportsGetRequest = {
-		orgId: getIdForUser(user, "org")
-	}
+  const { user } = useContext(UserContext);
 
-	const [loading, error, reports] = useApi(
-		api => api.apiOrganizationsOrgIdReportsGet(params),
-		[user]
-	);
+  const params: ApiOrganizationsOrgIdReportsGetRequest = {
+    orgId: getIdForUser(user, "org")
+  };
 
-	const reportsNeedAttention = 
-		!loading &&
-		!error &&
-		reports &&
-		reports.filter(r => !r.submittedAt).length > 0;
+  const [cacheInvalidator, setCacheInvalidator] = useState(0);
+  const appContext = {
+    invalidateCache: () => setCacheInvalidator((prev) => prev + 1)
+  };
 
-	const navItems: NavItemProps[] = [
-		{ type: 'primary', title: 'Roster', path: '/roster' },
-		{
-			type: 'primary',
-			title: 'Reports',
-			path: '/reports',
-			attentionNeeded: !!reportsNeedAttention,
-		},
-		// { type: 'secondary', title: 'Feedback', path: '/feedback' },
-		// { type: 'secondary', title: 'Help', path: '/help' },
-	];
+  const [loading, error, reports] = useApi(
+    api => api.apiOrganizationsOrgIdReportsGet(params),
+    [user, cacheInvalidator]
+  );
 
-	return (
-		<div className="App">
-			<a className="usa-skipnav" href="#main-content">
-				Skip to main content
-			</a>
-			<Header
-				title="Enrollment &amp; Funding"
-				navItems={navItems}
-				loginPath="/login"
-				logoutPath="/logout"
-				userFirstName={(user && user.firstName) || undefined }
-			/>
-			<main id="main-content">
-				<Switch>
-					{routes.map((route, index) => (
-						<MakeRouteWithSubRoutes key={index} {...route} />
-					))}
-				</Switch>
-			</main>
-		</div>
-	);
+  const pendingReportsCount =
+    !loading &&
+    !error &&
+    reports &&
+    reports.filter(r => !r.submittedAt).length;
+
+  const navItems: NavItemProps[] = [
+    { type: 'primary', title: 'Roster', path: '/roster' },
+    {
+      type: 'primary',
+      title: pendingReportsCount && pendingReportsCount > 0 ? `Reports (${pendingReportsCount})` : 'Reports',
+      path: '/reports',
+    },
+    // { type: 'secondary', title: 'Feedback', path: '/feedback' },
+    // { type: 'secondary', title: 'Help', path: '/help' },
+  ];
+
+  return (
+    <div className="App">
+      <AppContext.Provider value={appContext}>
+        <a className="usa-skipnav" href="#main-content">
+          Skip to main content
+        </a>
+        <Header
+          title="Enrollment &amp; Funding"
+          navItems={navItems}
+          loginPath="/login"
+          logoutPath="/logout"
+          userFirstName={(user && user.firstName) || undefined}
+        />
+        <main id="main-content">
+          <Switch>
+            {routes.map((route, index) => (
+              <MakeRouteWithSubRoutes key={index} {...route} />
+            ))}
+          </Switch>
+        </main>
+      </AppContext.Provider>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Hedwig/ClientApp/src/utils/parseCurrencyFromString.ts
@@ -0,0 +1,7 @@
+export default function parseCurrencyFromString(string: string) {
+  const undecoratedString = string.replace(/[^0-9.]/g, '');
+  const parsedFloat = parseFloat(undecoratedString);
+  const roundedFloat = Math.round(parsedFloat * 100) / 100;
+
+  return roundedFloat || null;
+}
