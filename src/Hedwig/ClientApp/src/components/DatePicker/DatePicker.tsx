f81309e5ef78017039233d1dcb6f81ea2328c071
Melanie <melanie.mazanec@gmail.com>
2019-12-24 23:32:07 -0500

    Delete irrelevant notes

--- src/Hedwig/ClientApp/src/components/DatePicker/DatePicker.tsx
+++ src/Hedwig/ClientApp/src/components/DatePicker/DatePicker.tsx
@@ -50,14 +50,14 @@ export const DatePicker: React.FC<DatePickerProps> = ({
 	}
 
 	// Make date picker take id value so people can add a label nearby?-- see enrollment use cases
-	// Just add a label here instead of using fieldset?
-	// Is this an appropriate use of fieldset?
-	// "Many assistive technologies will use the <legend> element as if it is a part of the label of each widget inside the corresponding <fieldset> element. For example, some screen readers such as Jaws or NVDA will speak the legend's content before speaking the label of each widget."
+	// Should we only use fieldset when it's a date range picker?
+		// "Many assistive technologies will use the <legend> element as if it is a part of the label of each widget inside the corresponding <fieldset> element. For example, some screen readers such as Jaws or NVDA will speak the legend's content before speaking the label of each widget."
 
 	// Also: accept people entering a date like 6/22/89
+
 	// Override js defaults if necessary to assume 2000s rather than 1900s!
 
-	// Make it so that user can optionally make calendar not show for things like birthdate?  But keep format for consistency's sake?
+	// TODO: Make it so that user can optionally make calendar not show for things like birthdate?  But keep format for consistency's sake?
 
 	// Date range input fields have aria label by default
 	return (
