a3c9b85a5e2cca07a43e0af6ce4307723f982418
Melanie <melanie.mazanec@gmail.com>
2019-12-26 11:52:17 -0500

    Convert disorganized notes into todos

--- src/Hedwig/ClientApp/src/components/DatePicker/DatePicker.tsx
+++ src/Hedwig/ClientApp/src/components/DatePicker/DatePicker.tsx
@@ -49,17 +49,17 @@ export const DatePicker: React.FC<DatePickerProps> = ({
 		return false;
 	}
 
-	// Make date picker take id value so people can add a label nearby?-- see enrollment use cases
-	// Should we only use fieldset when it's a date range picker?
-		// "Many assistive technologies will use the <legend> element as if it is a part of the label of each widget inside the corresponding <fieldset> element. For example, some screen readers such as Jaws or NVDA will speak the legend's content before speaking the label of each widget."
+	// TODO: Make date picker take id value so people can add a label nearby?-- see enrollment use cases
+	// OR just include label here?  Or make fieldset legend look like a label in some cases, like this one?
 
-	// Also: accept people entering a date like 6/22/89
+	// TODO: Should we only use fieldset when it's a date range picker?
 
-	// Override js defaults if necessary to assume 2000s rather than 1900s!
+	// TODO: Accept people entering a date like 6/22/89
+
+	// TODO: Override js defaults if necessary to assume 2000s rather than 1900s!
 
 	// TODO: Make it so that user can optionally make calendar not show for things like birthdate?  But keep format for consistency's sake?
 
-	// Date range input fields have aria label by default
 	return (
 		<FieldSet legend={legend} error={error} id={id}>
 			{byRange && (
