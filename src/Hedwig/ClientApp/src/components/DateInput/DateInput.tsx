2ba0b19add7ca301a15967f327235447b292b16c
Melanie <melanie.mazanec@gmail.com>
2019-12-31 20:40:12 -0500

    Rip date calendar out of input

--- src/Hedwig/ClientApp/src/components/DateInput/DateInput.tsx
+++ src/Hedwig/ClientApp/src/components/DateInput/DateInput.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import moment, { Moment } from 'moment';
-import { DateRangePicker, SingleDatePicker } from 'react-dates';
 import FieldSet from '../FieldSet/FieldSet';
 import TextInput from '../TextInput/TextInput';
 import FormStatus, { FormStatusProps } from '../FormStatus/FormStatus';
@@ -32,7 +31,7 @@ type DatePickerProps = {
 	onChange: (newRange: DateRange) => void;
 	id: string;
 	label: string | JSX.Element;
-	format: 'rangeCalendar' | 'dayCalendar' | 'dayInput' | 'rangeInput';
+	format: 'dayInput' | 'rangeInput';
 	onBlur?: (currentProps: DatePickerProps) => void;
 	disabled?: boolean;
 	status?: FormStatusProps;
@@ -128,56 +127,6 @@ export const DatePicker: React.FC<DatePickerProps> = ({
 		return false;
 	}
 
-	if (format === 'rangeCalendar') {
-		return (
-			<FieldSet legend={label} status={status} id={id} showLegend={!hideLabel}>
-				<DateRangePicker
-					startDate={selectedRange.startDate}
-					startDateId={`${id}-start-date`}
-					endDate={selectedRange.endDate}
-					endDateId={`${id}-end-date`}
-					focusedInput={datePickerFocused}
-					onDatesChange={dates => setDateRange(dates)}
-					onFocusChange={(focused: string | null) => setDatePickerFocused(focused)}
-					isOutsideRange={date => isOutsidePossibleRange(date)}
-					initialVisibleMonth={() => moment().subtract(1, 'M')}
-					numberOfMonths={1}
-					noBorder={true}
-					disabled={disabled}
-				/>
-			</FieldSet>
-		);
-	}
-	if (format === 'dayCalendar') {
-		return (
-			<div className={`usa-form-group ${status ? ` usa-form-group--${status.type}` : ''}`}>
-				<label
-					className={`usa-label margin-top-0 ${status ? ` usa-label--${status.type}` : ''}`}
-					htmlFor={`${id}-date`}
-				>
-					{label}
-				</label>
-				{status && <FormStatus {...status} />}
-				<span
-					className={`oec-date-input${status ? ` oec-date-input--${status.type}` : ''}`}
-					aria-describedby={status ? status.id : undefined}
-				>
-					<SingleDatePicker
-						id={`${id}-date`}
-						date={selectedRange.startDate}
-						focused={datePickerFocused === 'startDate'}
-						onDateChange={date => setDateRange({ startDate: date, endDate: date })}
-						onFocusChange={({ focused }: any) => setDatePickerFocused(focused ? 'startDate' : null)}
-						isOutsideRange={date => isOutsidePossibleRange(date)}
-						initialVisibleMonth={() => moment().subtract(1, 'M')}
-						numberOfMonths={1}
-						noBorder={true}
-						disabled={disabled}
-					/>
-				</span>
-			</div>
-		);
-	}
 	// OnChange-- do it one input box at a time?  Autofocus?  Research this
 	// TODO: Accept people entering a date like 6/22/89-- override js defaults if necessary to assume 2000s rather than 1900s!
 	// TODO: revisit usage of this datepicker library at all-- can't add aria-describedby :/
