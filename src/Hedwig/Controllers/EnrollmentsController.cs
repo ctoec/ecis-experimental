9265d4ee62c4eb0d153643a32297aad62e191661
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2020-01-13 12:46:35 -0500

    Remove uncessary async keywork (PR #612) (Issue #206)
    Rider: clean up unnecessary using statements

--- src/Hedwig/Controllers/EnrollmentsController.cs
+++ src/Hedwig/Controllers/EnrollmentsController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Hedwig.Models;
 using Hedwig.Repositories;
--- src/Hedwig/Security/RequirementsHandler.cs
+++ src/Hedwig/Security/RequirementsHandler.cs
@@ -1,9 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
-using System;
 using System.Linq;
-using System.Security.Claims;
 using System.Threading.Tasks;
 using Hedwig.Repositories;
 
