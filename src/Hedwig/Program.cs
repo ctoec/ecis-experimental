3cef35696efa4e956e0d904d115e07b740e9a12d
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2020-01-15 16:20:14 -0500

    PR comments

--- src/Hedwig/Program.cs
+++ src/Hedwig/Program.cs
@@ -10,37 +10,37 @@ using Hedwig.Data;
 
 namespace Hedwig
 {
-  public class Program
-  {
-    protected Program() { }
+	public class Program
+	{
+		protected Program() { }
 
-    public static void Main(string[] args)
-    {
-      var host = CreateHostBuilder(args).Build();
-      var environment = GetEnvironmentNameFromAppSettings();
-      if(environment != Environments.Production) {
-        using (var scope = host.Services.CreateScope())
-        {
-          var services = scope.ServiceProvider;
-          try
-          {
-            var context = services.GetRequiredService<HedwigContext>();
-            var initializer = new DbInitializer(context);
-            initializer.Initialize();
-          }
-          catch (Exception ex)
-          {
-            var logger = services.GetRequiredService<ILogger<Program>>();
-            logger.LogError(ex, "An error occurred while seeding the database.");
-          }
-        }
-      }
+		public static void Main(string[] args)
+		{
+			var host = CreateHostBuilder(args).Build();
+			var environment = GetEnvironmentNameFromAppSettings();
+			if(environment != Environments.Production) {
+				using (var scope = host.Services.CreateScope())
+				{
+					var services = scope.ServiceProvider;
+					try
+					{
+						var context = services.GetRequiredService<HedwigContext>();
+						var initializer = new DbInitializer(context);
+						initializer.Initialize();
+					}
+					catch (Exception ex)
+					{
+						var logger = services.GetRequiredService<ILogger<Program>>();
+						logger.LogError(ex, "An error occurred while seeding the database.");
+					}
+				}
+			}
 
-      host.Run();
-    }
+			host.Run();
+		}
 
-    public static IWebHostBuilder CreateHostBuilder(string[] args) {
-      var environment = GetEnvironmentNameFromAppSettings();
+		public static IWebHostBuilder CreateHostBuilder(string[] args) {
+			var environment = GetEnvironmentNameFromAppSettings();
 			return WebHost.CreateDefaultBuilder(args)
 			.ConfigureLogging((context, logging) =>
 			{
@@ -62,17 +62,17 @@ namespace Hedwig
 
 		public static IConfigurationRoot GetIConfigurationRoot()
 		{
-	  return new ConfigurationBuilder()
+		return new ConfigurationBuilder()
 			.SetBasePath(Directory.GetCurrentDirectory())
 			.AddJsonFile("appsettings.json", optional: true)
 			.AddEnvironmentVariables()
 			.Build();
 		}
 
-    private static string GetEnvironmentNameFromAppSettings(string defaultValue = null)
-    {
-      return Program.GetIConfigurationRoot()
-        .GetValue<string>("EnvironmentName", defaultValue ?? "Development");
-    }
-  }
+		private static string GetEnvironmentNameFromAppSettings(string defaultValue = null)
+		{
+			return Program.GetIConfigurationRoot()
+				.GetValue<string>("EnvironmentName", defaultValue ?? "Development");
+		}
+	}
 }
--- src/Hedwig/Startup.cs
+++ src/Hedwig/Startup.cs
@@ -11,93 +11,94 @@ using Microsoft.OpenApi.Models;
 
 namespace Hedwig
 {
-  public class Startup
-  {
-    public IConfiguration Configuration { get; }
+	public class Startup
+	{
+		public IConfiguration Configuration { get; }
 
-    public Startup(IConfiguration configuration)
-    {
-      Configuration = configuration;
-    }
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-    public virtual void ConfigureServices(IServiceCollection services)
-    {
+		public virtual void ConfigureServices(IServiceCollection services)
+		{
 			services.ConfigureSqlServer(Configuration.GetConnectionString("HEDWIG"));
 			services.ConfigureCors();
-      services.ConfigureControllers();
-      services.ConfigureSpa();
-      services.ConfigureRepositories();
-      services.ConfigureAuthentication(Configuration.GetValue<string>("WingedKeysUri"));
-      services.ConfigureAuthorization();
-      services.ConfigureValidation();
-      services.AddSwaggerGen(c =>
-      {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Hedwig API", Version = "v1" });
-        c.DescribeAllEnumsAsStrings();
-        c.TagActionsBy(api => new List<string> { "Hedwig" });
-        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-        {
-          Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
-          Name = "Authorization",
-          In = ParameterLocation.Header,
-          Type = SecuritySchemeType.ApiKey,
-          Scheme = "Bearer"
-        });
-        c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-        {
-          {
-            new OpenApiSecurityScheme
-            {
-              Reference = new OpenApiReference
-              {
-                Type = ReferenceType.SecurityScheme,
-                Id = "Bearer"
-              },
-              Scheme = "oauth2",
-              Name = "Bearer",
-              In = ParameterLocation.Header
-            },
-            new List<string> { }
-          }
-        });
-      });
-      services.AddHttpContextAccessor();
-    }
+			services.ConfigureControllers();
+			services.ConfigureSpa();
+			services.ConfigureRepositories();
+			services.ConfigureAuthentication(Configuration.GetValue<string>("WingedKeysUri"));
+			services.ConfigureAuthorization();
+			services.ConfigureValidation();
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new OpenApiInfo { Title = "Hedwig API", Version = "v1" });
+				c.DescribeAllEnumsAsStrings();
+				c.TagActionsBy(api => new List<string> { "Hedwig" });
+				c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+				{
+					Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+					Name = "Authorization",
+					In = ParameterLocation.Header,
+					Type = SecuritySchemeType.ApiKey,
+					Scheme = "Bearer"
+				});
+				c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+				{
+					{
+						new OpenApiSecurityScheme
+						{
+							Reference = new OpenApiReference
+							{
+								Type = ReferenceType.SecurityScheme,
+								Id = "Bearer"
+							},
+							Scheme = "oauth2",
+							Name = "Bearer",
+							In = ParameterLocation.Header
+						},
+						new List<string> { }
+					}
+				});
+			});
+			services.AddHttpContextAccessor();
+		}
 
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-        IdentityModelEventSource.ShowPII = true;
-        app.UseCors();
-      }
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				IdentityModelEventSource.ShowPII = true;
+				app.UseCors();
+			}
 
-      app.UseHttpsRedirection();
+			app.UseHttpsRedirection();
 
-      app.UseSwagger();
-      app.UseRouting();
+			app.UseSwagger();
+			app.UseRouting();
 
-      app.UseAuthentication();
-      app.UseAuthorization();
+			app.UseAuthentication();
+			app.UseAuthorization();
 
 			if (!env.IsDevelopment())
-      {
-        app.UseSpaStaticFiles();
-      }
+			{
+				app.UseSpaStaticFiles();
+			}
 
-      app.UseEndpoints(endpoints =>
-      {
-        endpoints.MapControllers();
-      });
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
 
-      app.UseSpa(spa =>
-      {
-        spa.Options.SourcePath = "ClientApp";
+			app.UseSpa(spa =>
+			{
+				spa.Options.SourcePath = "ClientApp";
 
-        if (env.IsDevelopment())
-        {
-					if (Environment.GetEnvironmentVariable("DOCKER_DEVELOPMENT") != null)
+				if (env.IsDevelopment())
+				{
+					var isDocker = Environment.GetEnvironmentVariable("DOCKER_DEVELOPMENT");
+					if (isDocker == "true")
 					{
 						string CLIENT_HOST = Environment.GetEnvironmentVariable("CLIENT_HOST") ?? "http://localhost:3000";
 						spa.UseProxyToSpaDevelopmentServer(CLIENT_HOST);
@@ -105,10 +106,10 @@ namespace Hedwig
 					else
 					{
 						spa.UseReactDevelopmentServer(npmScript: "start");
-					}		  
+					}
 				}
-      });
+			});
 
-    }
-  }
+		}
+	}
 }
