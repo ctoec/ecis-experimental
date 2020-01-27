c7b3cf00147a358b8affb4c13b6103422d5a0da9
julia <julia@skylight.digital>
2020-01-27 14:37:03 -0500

    family determination tests

---
+++ src/Hedwig/ClientApp/src/utils/models/familyDetermination.test.ts
@@ -0,0 +1,98 @@
+import { Enrollment, Child, Family } from "../../generated"
+import { familyDeterminationNotDisclosed, determinationSorter } from "./familyDetermination";
+
+const baseEnrollment : Enrollment = {
+  id: 1,
+  siteId: 1,
+  childId: 'foo-bar',
+};
+
+const baseChild : Child = {
+  id: 'foo-bar',
+  firstName: 'first',
+  lastName: 'last',
+  organizationId: 1
+};
+
+const baseFamily : Family = {
+  id: 1,
+  organizationId: 1
+}
+
+it.each([[true, true], [false, false], [undefined, false]])
+('familyDeterminationNotDisclosed correctly assesses if family determination is not disclosed', (
+  notDisclosed, notDisclosedRes
+) => {
+  const determination = {
+    id: 1,
+    familyId: 1,
+    determinationDate: new Date('2019-01-01'),
+    notDisclosed: notDisclosed
+  };
+  
+  const enrollment = {
+    ...baseEnrollment,
+    child: {
+      ...baseChild,
+      family: {
+        ...baseFamily,
+        determinations: [
+          determination
+        ]
+      }
+    }
+  }
+  
+  const res = familyDeterminationNotDisclosed(enrollment);
+
+  expect(res).toBe(notDisclosedRes);
+});
+
+it.each([undefined, []])
+('familyDeterminationNotDisclosed returns false if no determinations', (
+  determinations
+) => {
+  const enrollment = {
+    ...baseEnrollment,
+    child: {
+      ...baseChild,
+      family: {
+        ...baseFamily,
+        determinations: determinations
+      }
+    }
+  };
+
+  const res = familyDeterminationNotDisclosed(enrollment);
+
+  expect(res).toBeFalsy();
+});
+
+it('determinationSorter sorts determinations by determinationDate', () => {
+  const noDateId = 0;
+  const noDate = {
+    id: noDateId,
+    familyId: 1,
+    determinationDate: undefined,
+  };
+
+  const earlierId = 1;
+  const earlier = {
+    id: earlierId,
+    familyId: 1,
+    determinationDate: new Date('2019-01-01')
+  };
+
+  const laterId = 2;
+  const later = {
+    id: laterId,
+    familyId: 1,
+    determinationDate: new Date('2019-10-01')
+  };
+
+  const determinations = [noDate, later, earlier];
+
+  determinations.sort(determinationSorter);
+
+  expect(determinations.map(det => det.id)).toStrictEqual([earlierId, laterId, noDateId])
+});
--- src/Hedwig/ClientApp/src/utils/models/familyDetermination.ts
+++ src/Hedwig/ClientApp/src/utils/models/familyDetermination.ts
@@ -1,17 +1,21 @@
 import idx from 'idx';
-import { Enrollment } from '../../generated';
+import { Enrollment, FamilyDetermination } from '../../generated';
 
-export const familyDeterminationNotDisclosed = (enrollment: Enrollment) : boolean => {
-	let determinations = idx(enrollment, _ => _.child.family.determinations)
+export function familyDeterminationNotDisclosed(enrollment: Enrollment): boolean {
+	let determinations = idx(enrollment, _ => _.child.family.determinations) as FamilyDetermination[];
 
 	// If no determinations are present, not disclosed = false
 	// (because it is not explicitly true)
 	if (!determinations || determinations.length === 0) return false;
-	determinations = determinations.sort((a, b) => {
-		if (a.determinationDate > b.determinationDate) return 1;
-		if (a.determinationDate < b.determinationDate) return -1;
-		return 0;
-	});
+	determinations = determinations.sort(determinationSorter);
 
-	return determinations[0].notDisclosed;
-};
+	return !!determinations[0].notDisclosed;
+}
+
+export function determinationSorter(a: FamilyDetermination, b: FamilyDetermination) {
+	if(!a.determinationDate) return 1;
+	if(!b.determinationDate) return -1;
+	if(a.determinationDate > b.determinationDate) return 1;
+	if(a.determinationDate < b.determinationDate) return -1;
+	return 0;
+}	
