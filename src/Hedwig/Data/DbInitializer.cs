854b57daee27fc100c8a213ca825f5de425e2b78
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2020-01-30 16:30:37 -0500

    Fix date initalization

--- src/Hedwig/Data/DbInitializer.cs
+++ src/Hedwig/Data/DbInitializer.cs
@@ -39,7 +39,7 @@ namespace Hedwig.Data
         CreateReportingPeriod(period: "2019-10-01", start: "2019-09-30", end: "2019-10-27", due: "2019-11-15"),
         CreateReportingPeriod(period: "2019-11-01", start: "2019-10-28", end: "2019-11-29", due: "2019-12-15"),
         CreateReportingPeriod(period: "2019-12-01", start: "2019-12-02", end: "2019-12-27", due: "2020-01-15"),
-        CreateReportingPeriod(period: "2020-01-01", start: "2019-12-30", end: "2020-01-29", due: "2020-02-15"), // TO CHANGE!!!
+        CreateReportingPeriod(period: "2020-01-01", start: "2019-12-30", end: "2020-01-31", due: "2020-02-15"),
         CreateReportingPeriod(period: "2020-02-01", start: "2020-02-03", end: "2020-02-28", due: "2020-03-15")
       };
 
