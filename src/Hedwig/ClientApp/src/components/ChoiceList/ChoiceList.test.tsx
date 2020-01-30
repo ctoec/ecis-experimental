fb5edd1839f5c4b0bd98b65ede7c530ab618d2e3
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2020-01-30 16:53:33 -0500

    Remove dupe code

--- src/Hedwig/ClientApp/src/components/ChoiceList/ChoiceList.test.tsx
+++ src/Hedwig/ClientApp/src/components/ChoiceList/ChoiceList.test.tsx
@@ -2,113 +2,64 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { ChoiceList } from './ChoiceList';
 
+function createChoiceList(type: 'select' | 'radio' | 'check', selected?: string[]) {
+	const commonProps = {
+		id: 'id',
+		options: [
+			{ text: 'Option 1', value: '1' },
+			{ text: 'Option 2', value: '2' }
+		],
+		onChange: () => {}
+	}
+	if (type === 'select') {
+		return <ChoiceList
+			{...commonProps}
+			type={type}
+			label="choice"
+			selected={selected}
+		/>
+	} else {
+		return <ChoiceList
+			{...commonProps}
+			type={type}
+			legend="choice"
+			selected={selected}
+		/>
+	}
+}
+
 describe('select', () => {
 	it('matches snapshot', () => {
-		const component = render(
-			<ChoiceList
-				id="id"
-				type="select"
-				label="choice"
-				options={[
-					{ text: 'Option 1', value: '1' },
-					{ text: 'Option 2', value: '2' }
-				]}
-				onChange={() => {}}
-			/>
-		);
-	
+		const component = render(createChoiceList('select'));
 		expect(component).toMatchSnapshot();
 	});
 
 	it('selected prop is the selected input', () => {
-		const component = render(
-			<ChoiceList
-				id="id"
-				type="select"
-				label="choice"
-				options={[
-					{ text: 'Option 1', value: '1' },
-					{ text: 'Option 2', value: '2' }
-				]}
-				onChange={() => {}}
-				selected={['1']}
-			/>
-		);
-
+		const component = render(createChoiceList('select', ['1']));
 		expect(component.getByDisplayValue(/Option 1/i)).toBeTruthy();
 	});
 });
 
 describe('check', () => {
 	it('matches snapshot', () => {
-		const component = render(
-			<ChoiceList
-				id="id"
-				type="check"
-				legend="choice"
-				options={[
-					{ text: 'Option 1', value: '1' },
-					{ text: 'Option 2', value: '2' }
-				]}
-				onChange={() => {}}
-			/>
-		);
-	
+		const component = render(createChoiceList('check'));
 		expect(component).toMatchSnapshot();
 	});
 
 	it('selected prop is the selected input', () => {
-		const component = render(
-			<ChoiceList
-				id="id"
-				type="check"
-				legend="choice"
-				options={[
-					{ text: 'Option 1', value: '1' },
-					{ text: 'Option 2', value: '2' }
-				]}
-				onChange={() => {}}
-				selected={['1']}
-			/>
-		);
-
+		const component = render(createChoiceList('check', ['1']));
 		expect((component.getByDisplayValue(/1/i) as HTMLInputElement).checked).toBeTruthy()
 	});
 });
 
 describe('radio', () => {
 	it('matches snapshot', () => {
-		const component = render(
-			<ChoiceList
-				id="id"
-				type="radio"
-				legend="choice"
-				options={[
-					{ text: 'Option 1', value: '1' },
-					{ text: 'Option 2', value: '2' }
-				]}
-				onChange={() => {}}
-			/>
-		);
-	
+		const component = render(createChoiceList('radio'));
 		expect(component).toMatchSnapshot();
 	});
 
 	it('selected prop is the selected input', () => {
-		const component = render(
-			<ChoiceList
-				id="id"
-				type="radio"
-				legend="choice"
-				options={[
-					{ text: 'Option 1', value: '1' },
-					{ text: 'Option 2', value: '2' }
-				]}
-				onChange={() => {}}
-				selected={['1']}
-			/>
-		);
-
+		const component = render(createChoiceList('radio', ['1']));
 		expect((component.getByDisplayValue(/1/i) as HTMLInputElement).checked).toBeTruthy()
 	});
 });
