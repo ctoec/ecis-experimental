c385d90dfcc9782efad49571fe7ac4e21db95122
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2020-02-04 14:11:46 -0500

    PR comments

--- src/Hedwig/ClientApp/src/components/Header/Header.test.tsx
+++ src/Hedwig/ClientApp/src/components/Header/Header.test.tsx
@@ -18,49 +18,16 @@ const headerProps: HeaderProps = {
 	logoutPath: '/logout',
 };
 
-it('shows first section as active when at the root path', () => {
-	const { getByText } = render(
-		<MemoryRouter>
-			<Header {...headerProps} />
-		</MemoryRouter>
-	);
-
-	const activeSection = getByText('Active section').closest('a');
-	const otherSection = getByText('Another section').closest('a');
-	expect(activeSection).toBeDefined();
-	expect(otherSection).toBeDefined();
-	if (!activeSection || !otherSection) {
+const getClosestAnchorAndValidateExists = (get: any, text: string | RegExp): Exclude<any, undefined> => {
+	const section = get(text).closest('a');
+	expect(section).toBeDefined();
+	if (!section) {
 		throw new Error('Typescript guarding');
 	}
-	expect(activeSection.classList.contains('usa-current')).toBeTruthy();
-	expect(otherSection.classList.contains('usa-current')).toBeFalsy();
-});
+	return section;
+}
 
-it('shows the path as active when matching', () => {
-	const { getByText } = render(
-		<MemoryRouter initialEntries={['/another/nested/path']}>
-			<Header {...headerProps} />
-		</MemoryRouter>
-	);
-
-	const activeSection = getByText('Active section').closest('a');
-	const otherSection = getByText('Another section').closest('a');
-	expect(activeSection).toBeDefined();
-	expect(otherSection).toBeDefined();
-	if (!activeSection || !otherSection) {
-		throw new Error('Typescript guarding');
-	}
-	expect(activeSection.classList.contains('usa-current')).toBeFalsy();
-	expect(otherSection.classList.contains('usa-current')).toBeTruthy();
-});
-
-it('opens and closes menu when menu clicked', () => {
-	const { getByText, getByRole, getByTestId, getByAltText } = render(
-		<MemoryRouter>
-			<Header {...headerProps} />
-		</MemoryRouter>
-	);
-
+const getMenuElementsAndValidateOpenMenu = (getByText: any, getByRole: any, getByTestId: any) => {
 	const menu = getByText(/menu/i);
 	const overlay = getByTestId('overlay');
 	const nav = getByRole(/navigation/i);
@@ -80,9 +47,43 @@ it('opens and closes menu when menu clicked', () => {
 
 	expect(overlay.classList.contains('is-visible')).toBeTruthy();
 	expect(nav.classList.contains('is-visible')).toBeTruthy();
+	return { menu, overlay, nav };
+};
+
+it('shows first section as active when at the root path', () => {
+	const { getByText } = render(
+		<MemoryRouter>
+			<Header {...headerProps} />
+		</MemoryRouter>
+	);
+	const activeSection = getClosestAnchorAndValidateExists(getByText, /Active Section/i);
+	const otherSection = getClosestAnchorAndValidateExists(getByText, /Another Section/i);
+	expect(activeSection.classList.contains('usa-current')).toBeTruthy();
+	expect(otherSection.classList.contains('usa-current')).toBeFalsy();
+});
+
+it('shows the path as active when matching', () => {
+	const { getByText } = render(
+		<MemoryRouter initialEntries={['/another/nested/path']}>
+			<Header {...headerProps} />
+		</MemoryRouter>
+	);
+	const activeSection = getClosestAnchorAndValidateExists(getByText, /Active Section/i);
+	const otherSection = getClosestAnchorAndValidateExists(getByText, /Another Section/i);
+	expect(activeSection.classList.contains('usa-current')).toBeFalsy();
+	expect(otherSection.classList.contains('usa-current')).toBeTruthy();
+});
+
+it('opens and closes menu when menu clicked', () => {
+	const { getByText, getByRole, getByTestId, getByAltText } = render(
+		<MemoryRouter>
+			<Header {...headerProps} />
+		</MemoryRouter>
+	);
+
+	const { overlay, nav } = getMenuElementsAndValidateOpenMenu(getByText, getByRole, getByTestId);
 
 	const close = getByAltText(/close/i);
-
 	fireEvent.click(close);
 
 	expect(overlay.classList.contains('is-visible')).toBeFalsy();
@@ -97,25 +98,7 @@ it('closes menu when location changed', () => {
 		</Router>
 	);
 
-	const menu = getByText(/menu/i);
-	const overlay = getByTestId('overlay');
-	const nav = getByRole(/navigation/i);
-	// TODO: (After CSS refactor) Ensure the menu is not visible
-	expect(overlay.classList.contains('is-visible')).toBeFalsy();
-	expect(nav.classList.contains('is-visible')).toBeFalsy();
-
-	// Resize window so that menu button appears
-	act(() => {
-		// @ts-ignore TS does not allow changing innerWidth
-		window.innerWidth = 100;
-		fireEvent(window, new Event('resize'));
-	});
-
-	// TODO: (After CSS refactor) Ensure the menu is visible
-	fireEvent.click(menu);
-
-	expect(overlay.classList.contains('is-visible')).toBeTruthy();
-	expect(nav.classList.contains('is-visible')).toBeTruthy();
+	const { overlay, nav } = getMenuElementsAndValidateOpenMenu(getByText, getByRole, getByTestId);
 
 	act(() => {
 		history.push('/reports');
@@ -126,31 +109,13 @@ it('closes menu when location changed', () => {
 });
 
 it('closes menu when clicking elsewhere', () => {
-	const { getByText, getByRole, getByTestId, getByAltText } = render(
+	const { getByText, getByRole, getByTestId } = render(
 		<MemoryRouter>
 			<Header {...headerProps} />
 		</MemoryRouter>
 	);
 
-	const menu = getByText(/menu/i);
-	const overlay = getByTestId('overlay');
-	const nav = getByRole(/navigation/i);
-	// TODO: (After CSS refactor) Ensure the menu is not visible
-	expect(overlay.classList.contains('is-visible')).toBeFalsy();
-	expect(nav.classList.contains('is-visible')).toBeFalsy();
-
-	// Resize window so that menu button appears
-	act(() => {
-		// @ts-ignore TS does not allow changing innerWidth
-		window.innerWidth = 100;
-		fireEvent(window, new Event('resize'));
-	});
-
-	// TODO: (After CSS refactor) Ensure the menu is visible
-	fireEvent.click(menu);
-
-	expect(overlay.classList.contains('is-visible')).toBeTruthy();
-	expect(nav.classList.contains('is-visible')).toBeTruthy();
+	const { overlay, nav } = getMenuElementsAndValidateOpenMenu(getByText, getByRole, getByTestId);
 
 	fireEvent.click(overlay);
 
