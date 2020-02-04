789c8259a22b2a1f69ac6eef112054180f4d81e3
Melanie <melanie.mazanec@gmail.com>
2020-02-04 09:51:34 -0500

    Remove cruft from on route change

--- src/Hedwig/ClientApp/src/hooks/onRouteChange.ts
+++ src/Hedwig/ClientApp/src/hooks/onRouteChange.ts
@@ -4,8 +4,6 @@ import { useLocation } from 'react-router-dom';
 export default function onRouteChange(callback: () => any) {
 	const { pathname } = useLocation();
 	useEffect(() => {
-		window.scrollTo(0, 0);
 		callback();
 	}, [pathname]);
-	return null;
 }
