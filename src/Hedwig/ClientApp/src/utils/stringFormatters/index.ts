8141ae030798b8ba8bb2207d239bf56f5c0282d4
Melanie <melanie.mazanec@gmail.com>
2020-02-12 15:54:56 -0500

    Resolve rebase issues

--- src/Hedwig/ClientApp/src/utils/stringFormatters/index.ts
+++ src/Hedwig/ClientApp/src/utils/stringFormatters/index.ts
@@ -3,7 +3,6 @@ export { nameFormatter, lastFirstNameFormatter } from './nameFormatter';
 export { rosterEnrollmentsFormatter } from './rosterEnrollmentsFormatter';
 export { elementIdFormatter } from './elementIdFormatter';
 export { enrollmentDetailMetadataFormatter} from './enrollmentDetailMetadataFormatter';
-export { enrollmentTextFormatter } from './enrollmentTextFormatter';
 export {
 	newEnrollmentCompleteAlert,
 	newEnrollentMissingInfoAlert,
