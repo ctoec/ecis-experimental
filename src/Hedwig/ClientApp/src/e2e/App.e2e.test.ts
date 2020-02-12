d9021cd0d471e5d4f493d8ab0ae0666036ce3e4a
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2020-02-12 10:03:42 -0500

    Improve organization and some clean up
    Improve exit handling and docker compose structure

--- src/Hedwig/ClientApp/src/e2e/App.e2e.test.ts
+++ src/Hedwig/ClientApp/src/e2e/App.e2e.test.ts
@@ -1,41 +1,50 @@
-import { createDriver, cleanup } from './helpers';
+import { DriverHelper } from './helpers';
 import { app, clickLogin, waitForLoginWelcome, waitForHeader } from './pages/App';
 
 // Set time out to 30 seconds
 jest.setTimeout(30 * 1000);
 
+let driverHelper: DriverHelper;
+beforeAll(() => {
+	driverHelper = new DriverHelper();
+});
+
 describe('Smoke screen', () => {
 	it('Browser Title renders', async () => {
-		const driver = createDriver();
+		const driver = driverHelper.createDriver();
 		try {
 			await app(driver);
 			const title = await driver.getTitle()
 			expect(title).toBe('ECE Reporter');
 		} finally {
-			await cleanup(driver);
+			await driverHelper.quit(driver);
 		}
 	});
 	
 	it('HTML Title renders', async () => {
-		const driver = createDriver();
+		const driver = driverHelper.createDriver();
 		try {
 			await app(driver);
 			const header = await waitForHeader(driver);
 			expect(await header.getText()).toBe('ECE Reporter');
 		} finally {
-			await cleanup(driver);
+			await driverHelper.quit(driver);
 		}
 	});
 
 	it('Logs in', async () => {
-		const driver = createDriver();
+		const driver = driverHelper.createDriver();
 		try {
 			await app(driver);
 			await clickLogin(driver);
 			const name = await waitForLoginWelcome(driver);
 			expect(await name.getText()).toBe('Hi, Chris.');
 		} finally {
-			await cleanup(driver);
+			await driverHelper.quit(driver);
 		}
 	});
+});
+
+afterAll(() => {
+	driverHelper.cleanup();
 });
--- src/Hedwig/ClientApp/src/e2e/helpers.ts
+++ src/Hedwig/ClientApp/src/e2e/helpers.ts
@@ -5,24 +5,43 @@ export const clientHost = 'https://backend-for-selenium:5001';
 export const gridHost = 'http://selenium-hub:4444/wd/hub';
 export const defaultTimeout = 10000;
 
-export const createDriver = () => {
-	// If browser not specified, default to any registered firefox
-	const builder = new Builder().forBrowser('firefox');
-	// Get capabilities for default or user specified browser
-	const capabilities = builder.getCapabilities();
-	// Override to force accept insecure certs
-	// Needed because we use a developer-signed cert in Hedwig
-	capabilities.setAcceptInsecureCerts(true);
-	return new Builder()
-		.withCapabilities(capabilities)
-		.usingServer(gridHost)
-		.build();
-}
+export class DriverHelper {
+	__drivers: IWebDriver[] = [];
 
-export const cleanup = async (driver: ThenableWebDriver | WebDriver) => {
-	await driver.quit();
-}
+	createDriver = () => {
+		// If browser not specified, default to any registered chrome
+		const builder = new Builder().forBrowser('chrome');
+		// Get capabilities for default or user specified browser
+		const capabilities = builder.getCapabilities();
+		// Override to force accept insecure certs
+		// Needed because we use a developer-signed cert in Hedwig
+		capabilities.setAcceptInsecureCerts(true);
+		const driver = new Builder()
+			.withCapabilities(capabilities)
+			.usingServer(gridHost)
+			.build();
+		this.__drivers.push(driver);
+		return driver;
+	}
 
+	quit = async (driver: ThenableWebDriver | WebDriver) => {
+		try {
+			await driver.quit();
+		} catch {
+			// Already closed
+		}
+	}
+
+	cleanup = async () => {
+		this.__drivers.forEach(async driver => {
+			try {
+				await driver.quit();
+			} catch {
+				// Already closed
+			}
+		});
+	}
+}
 export const loadAndWaitFor = async (driver: IWebDriver, selector: Locator) => {
 	await driver.get(clientHost);
 	return await waitFor(driver, selector);
