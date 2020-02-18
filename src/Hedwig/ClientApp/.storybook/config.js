0d15a9ec5b1294b4cb38edbeec3a23be9d4b0354
Melanie <melanie.mazanec@gmail.com>
2020-02-18 18:06:19 -0500

    Configure storyboook a11y

--- src/Hedwig/ClientApp/.storybook/config.js
+++ src/Hedwig/ClientApp/.storybook/config.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { configure, addDecorator } from '@storybook/react';
 import StoryRouter from 'storybook-react-router';
 import requireContext from 'require-context.macro';
+import { withA11y } from '@storybook/addon-a11y';
 
 import 'react-dates/lib/css/_datepicker.css';
 import '../src/assets/styles/index.scss';
 
 addDecorator(StoryRouter());
+addDecorator(withA11y());
 
 addDecorator(story => <div style={{ padding: '40px' }}>{story()}</div>)
 
