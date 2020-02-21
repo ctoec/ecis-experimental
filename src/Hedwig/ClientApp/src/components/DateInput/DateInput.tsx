4408dc2646e8cfb7e656909c1f7443d5fcf315b3
Melanie <melanie.mazanec@gmail.com>
2020-02-21 18:49:43 -0500

    Add date input

--- src/Hedwig/ClientApp/src/components/DateInput/DateInput.tsx
+++ src/Hedwig/ClientApp/src/components/DateInput/DateInput.tsx
@@ -1,32 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import moment, { Moment } from 'moment';
-import { FieldSet, TextInput, FormStatusProps } from '..';
+import { DayPickerRangeController, DayPickerSingleDateController } from 'react-dates';
+import { FieldSet, TextInput, FormStatusProps, Button } from '..';
 
 type DateRange = {
+	// TODO: do we want to deal exclusively in range or should we accept just a date too?  Should we make a separate range component?
 	startDate: Moment | null;
 	endDate: Moment | null;
 };
-
-type inputDetailType = {
-	props: {
-		label: string;
-		inputProps: any;
-	};
-	parseMoment: (inputMoment: Moment | null) => string | undefined;
-	checkValidity: (value: any) => boolean;
-	errorMessage: string;
-};
-
-type inputDetailsType = { [key: string]: inputDetailType };
-
-type rangeValType = {
-	start?: string;
-	end?: string;
-	startIsInvalid?: boolean;
-	endIsInvalid?: boolean;
-};
-
-type rangeByValType = { [key: string]: rangeValType };
 
 type DateInputProps = {
 	dateRange: DateRange;
@@ -42,38 +23,17 @@ type DateInputProps = {
 	// Will only take effect on fieldsets-- otherwise we should not hide the label
 };
 
-const inputDetails: inputDetailsType = {
-	month: {
-		props: {
-			inputProps: { maxLength: 2, type: 'number', min: 1, max: 12 },
-			label: 'Month',
-		},
-		parseMoment: (inputMoment: Moment | null) =>
-			inputMoment ? inputMoment.format('MM') : undefined,
-		checkValidity: value => +value > 0 && +value < 13,
-		errorMessage: 'Invalid month',
-	},
-	day: {
-		props: {
-			label: 'Day',
-			inputProps: { maxLength: 2, type: 'number', min: 1, max: 31 },
-		},
-		parseMoment: (inputMoment: Moment | null) =>
-			inputMoment ? inputMoment.format('DD') : undefined,
-		checkValidity: value => +value < 32 && +value > 0,
-		errorMessage: 'Invalid day',
-	},
-	year: {
-		props: {
-			label: 'Year',
-			inputProps: { maxLength: 4, type: 'number', min: 1990, max: 2100 },
-		},
-		parseMoment: (inputMoment: Moment | null) =>
-			inputMoment ? inputMoment.format('YYYY') : undefined,
-		checkValidity: value =>
-			value.length === 2 || (value.length === 4 && +value > 1989 && +value < 2101),
-		errorMessage: 'Invalid year',
-	},
+const parseDateInput = (input?: string): Moment | null => {
+	if (!input) return null;
+	const acceptedDelimiters = ['-', '/', ' '];
+	acceptedDelimiters.forEach(d => {
+		const splitInput = input.split(d);
+		if (splitInput.length === 3) {
+			// For parsing consistency across browsers
+			return moment(splitInput.join('-'), 'MM-DD-YYYY');
+		}
+	});
+	return null;
 };
 
 export const DateInput: React.FC<DateInputProps> = ({
@@ -86,153 +46,80 @@ export const DateInput: React.FC<DateInputProps> = ({
 	optional,
 	status,
 	className,
-	hideLabel,
 }) => {
-	const initialRangeByVal: rangeByValType = {};
-	Object.keys(inputDetails).forEach(dateItem => {
-		initialRangeByVal[dateItem] = {};
-		initialRangeByVal[dateItem].start = inputDetails[dateItem].parseMoment(dateRange.startDate);
-		initialRangeByVal[dateItem].end = inputDetails[dateItem].parseMoment(dateRange.endDate);
+	// On text input blur, check for validity of date
+	// Fire on change event either way?
+
+	// On calendar click, fire on change event, which should also update text input
+	// Make calendar dropdown optional
+
+	// TODO: default to empty date-- what should happen?
+
+	const [calendarOpen, setCalendarOpen] = useState<boolean>(false);
+	const [stringRange, setStringRange] = useState<{ startDate?: string; endDate?: string }>({
+		startDate: dateRange.startDate ? dateRange.startDate.format('MM/DD/YYYY') : undefined,
+		endDate: dateRange.endDate ? dateRange.endDate.format('DD/MM/YYYY') : undefined,
 	});
 
-	const [rangeByVal, setRangeByVal] = useState<rangeByValType>(initialRangeByVal);
-
-	// Add warning that date isn't valid if it isn't?
-	useEffect(() => {
-		const newStart = moment(
-			`${rangeByVal.year.start}-${rangeByVal.month.start}-${rangeByVal.day.start}`,
-			'YYYY-MM-DD'
-		);
-		let newEnd = newStart;
-		if (format === 'rangeInput') {
-			newEnd = moment(
-				`${rangeByVal.year.end}-${rangeByVal.month.end}-${rangeByVal.day.end}`,
-				'YYYY-MM-DD'
-			);
-		}
-		onChange({ startDate: newStart, endDate: newEnd });
-	}, [rangeByVal, format, onChange]);
-
-	// TODO: implement "optional" styling for fieldset
-
-	const commonDateInputProps = {
-		className: 'oec-date-input__input',
-		disabled: disabled,
-		type: 'number',
-		inline: true,
-	};
-
-	const showInlineWarning =
-		!status &&
-		Object.keys(rangeByVal).find(
-			dateItem => rangeByVal[dateItem].startIsInvalid || rangeByVal[dateItem].endIsInvalid
-		);
-	const inlineWarning: FormStatusProps | undefined = showInlineWarning
-		? { type: 'warning', id: 'invalid-date-field' }
-		: undefined;
-
-	const startDateFieldset = (
+	return (
 		<FieldSet
-			legend={format === 'rangeInput' ? `${label} start` : label}
-			id={`${id}-start-date`}
-			showLegend={format === 'rangeInput' ? true : !hideLabel}
-			hint={format !== 'rangeInput' ? 'For example: 04 28 1986' : ''}
-			childrenGroupClassName="flex-row flex-align-end usa-memorable-date"
-			className={className}
-			status={format === 'rangeInput' ? undefined : status || inlineWarning}
+			legend={label}
+			id={`${id}-fieldset`}
+			showLegend={format === 'rangeInput'}
+			disabled={disabled}
+			hint="For example: 04/28/1986"
+			status={status}
+			optional={optional}
 		>
-			{Object.keys(inputDetails).map(key => (
-				<div key={key}>
+			{format === 'dayInput' && (
+				<div className="grid-row flex-row flex-align-end grid-gap position-relative">
 					<TextInput
-						//@ts-ignore IDK, y'all TS has a problem here
-						type="input"
-						defaultValue={rangeByVal[key].start}
-						onChange={event => {
-							const newRangeVals = Object.assign({}, rangeByVal);
-							newRangeVals[key].start = event.target.value;
-							setRangeByVal(newRangeVals);
+						label={label}
+						id={id}
+						onChange={e => setStringRange({ startDate: e.target.value, endDate: e.target.value })}
+						defaultValue={stringRange.startDate}
+						onBlur={() => {
+							const parsedInput = parseDateInput(stringRange.startDate);
+							onChange({ startDate: parsedInput, endDate: parsedInput });
 						}}
-						id={`${id}-${key}-start-date`}
-						{...inputDetails[key].props}
-						{...commonDateInputProps}
-						onBlur={event => {
-							const newRangeVals = Object.assign({}, rangeByVal);
-							newRangeVals[key].startIsInvalid = !inputDetails[key].checkValidity(
-								event.target.value
-							);
-							setRangeByVal(newRangeVals);
-						}}
-						optional={optional}
-						status={
-							rangeByVal[key].startIsInvalid
-								? {
-										type: 'warning',
-										message: inputDetails[key].errorMessage,
-										id: `${id}-${key}-start-error`,
-								  }
-								: undefined
-						}
 					/>
+					<div>
+						<Button text={'Calendar'} onClick={() => setCalendarOpen(!calendarOpen)} />
+						{calendarOpen && (
+							<div className="position-absolute z-top">
+								<DayPickerSingleDateController
+									date={dateRange.startDate}
+									onDateChange={newDate => onChange({ startDate: newDate, endDate: newDate })}
+									focused={calendarOpen || false}
+									// Annoyingly this does not do anything for keyboard users
+									onFocusChange={f => setCalendarOpen(f.focused || false)}
+									onOutsideClick={(e) => {
+										const targetText = e.target.innerHTML;
+										if (targetText!== 'Calendar') {
+											setCalendarOpen(false)
+										}
+									}}
+								/>
+							</div>
+						)}
+					</div>
 				</div>
-			))}{' '}
+			)}
+			{/* {format === 'rangeInput' && (
+				// TODO: if line must break then arrow is on first line and calendar is on second
+				<>
+					<TextInput label="Start date" id={`${id}-start`} />
+					<span>TODO: PUT AN ARROW HERE</span>
+					<TextInput label="End date" id={`${id}-end`} />
+					<DayPickerRangeController
+						startDate={dateRange.startDate}
+						endDate={dateRange.endDate}
+						onDatesChange={onChange}
+						focusedInput={calendarOpen}
+						onFocusChange={setCalendarOpen}
+					/>
+				</>
+			)} */}
 		</FieldSet>
 	);
-
-	if (format === 'rangeInput') {
-		return (
-			<FieldSet
-				legend={label}
-				status={status || inlineWarning}
-				id={id}
-				showLegend={!hideLabel}
-				hint="For example: 04 28 1986"
-				childrenGroupClassName="flex-row display-flex flex-align-end"
-				className={className}
-			>
-				{startDateFieldset}
-				<FieldSet
-					legend={`${label} end`}
-					id={`${id}-end-date`}
-					showLegend
-					childrenGroupClassName="flex-row flex-align-end usa-memorable-date"
-				>
-					{Object.keys(inputDetails).map(key => (
-						<div key={key}>
-							<TextInput
-								//@ts-ignore IDK, y'all TS has a problem here
-								type="input"
-								defaultValue={rangeByVal[key].end}
-								onChange={event => {
-									const newRangeVals = Object.assign({}, rangeByVal);
-									newRangeVals[key].end = event.target.value;
-									setRangeByVal(newRangeVals);
-								}}
-								id={`${id}-${key}-end-date`}
-								{...inputDetails[key].props}
-								{...commonDateInputProps}
-								onBlur={event => {
-									const newRangeVals = Object.assign({}, rangeByVal);
-									newRangeVals[key].endIsInvalid = !inputDetails[key].checkValidity(
-										event.target.value
-									);
-									setRangeByVal(newRangeVals);
-								}}
-								optional={optional}
-								status={
-									rangeByVal[key].endIsInvalid
-										? {
-												type: 'warning',
-												message: inputDetails[key].errorMessage,
-												id: `${id}-${key}-start-error`,
-										  }
-										: undefined
-								}
-							/>
-						</div>
-					))}
-				</FieldSet>
-			</FieldSet>
-		);
-	}
-	return startDateFieldset;
 };
---
+++ src/Hedwig/ClientApp/src/components/DateInput/OldDateInput.tsx
@@ -0,0 +1,234 @@
+import React, { useState, useEffect } from 'react';
+import moment, { Moment } from 'moment';
+import { FieldSet, TextInput, FormStatusProps } from '..';
+
+type DateRange = {
+	startDate: Moment | null;
+	endDate: Moment | null;
+};
+
+type inputDetailType = {
+	props: {
+		label: string;
+		inputProps: any;
+	};
+	parseMoment: (inputMoment: Moment | null) => string | undefined;
+	checkValidity: (value: any) => boolean;
+	errorMessage: string;
+};
+
+type inputDetailsType = { [key: string]: inputDetailType };
+
+type rangeValType = {
+	start?: string;
+	end?: string;
+	startIsInvalid?: boolean;
+	endIsInvalid?: boolean;
+};
+
+type rangeByValType = { [key: string]: rangeValType };
+
+type DateInputProps = {
+	dateRange: DateRange;
+	onChange: (newRange: DateRange) => void;
+	id: string;
+	label: string;
+	format?: 'dayInput' | 'rangeInput';
+	disabled?: boolean;
+	optional?: boolean;
+	status?: FormStatusProps;
+	className?: string;
+	hideLabel?: boolean;
+	// Will only take effect on fieldsets-- otherwise we should not hide the label
+};
+
+const inputDetails: inputDetailsType = {
+	month: {
+		props: {
+			inputProps: { maxLength: 2, type: 'number', min: 1, max: 12 },
+			label: 'Month',
+		},
+		parseMoment: (inputMoment: Moment | null) =>
+			inputMoment ? inputMoment.format('MM') : undefined,
+		checkValidity: value => +value > 0 && +value < 13,
+		errorMessage: 'Invalid month',
+	},
+	day: {
+		props: {
+			label: 'Day',
+			inputProps: { maxLength: 2, type: 'number', min: 1, max: 31 },
+		},
+		parseMoment: (inputMoment: Moment | null) =>
+			inputMoment ? inputMoment.format('DD') : undefined,
+		checkValidity: value => +value < 32 && +value > 0,
+		errorMessage: 'Invalid day',
+	},
+	year: {
+		props: {
+			label: 'Year',
+			inputProps: { maxLength: 4, type: 'number', min: 1990, max: 2100 },
+		},
+		parseMoment: (inputMoment: Moment | null) =>
+			inputMoment ? inputMoment.format('YYYY') : undefined,
+		checkValidity: value =>
+			value.length === 2 || (value.length === 4 && +value > 1989 && +value < 2101),
+		errorMessage: 'Invalid year',
+	},
+};
+
+export const DateInput: React.FC<DateInputProps> = ({
+	dateRange,
+	onChange,
+	id,
+	label,
+	format = 'dayInput',
+	disabled,
+	optional,
+	status,
+	className,
+	hideLabel,
+}) => {
+	const initialRangeByVal: rangeByValType = {};
+	Object.keys(inputDetails).forEach(dateItem => {
+		initialRangeByVal[dateItem] = {};
+		initialRangeByVal[dateItem].start = inputDetails[dateItem].parseMoment(dateRange.startDate);
+		initialRangeByVal[dateItem].end = inputDetails[dateItem].parseMoment(dateRange.endDate);
+	});
+
+	const [rangeByVal, setRangeByVal] = useState<rangeByValType>(initialRangeByVal);
+
+	// Add warning that date isn't valid if it isn't?
+	useEffect(() => {
+		const newStart = moment(
+			`${rangeByVal.year.start}-${rangeByVal.month.start}-${rangeByVal.day.start}`,
+			'YYYY-MM-DD'
+		);
+		let newEnd = newStart;
+		if (format === 'rangeInput') {
+			newEnd = moment(
+				`${rangeByVal.year.end}-${rangeByVal.month.end}-${rangeByVal.day.end}`,
+				'YYYY-MM-DD'
+			);
+		}
+		onChange({ startDate: newStart, endDate: newEnd });
+	}, [rangeByVal, format, onChange]);
+
+	// TODO: implement "optional" styling for fieldset
+
+	const commonDateInputProps = {
+		className: 'oec-date-input__input',
+		disabled: disabled,
+		type: 'number',
+		inline: true,
+	};
+
+	const showInlineWarning =
+		!status &&
+		Object.keys(rangeByVal).find(
+			dateItem => rangeByVal[dateItem].startIsInvalid || rangeByVal[dateItem].endIsInvalid
+		);
+	const inlineWarning: FormStatusProps | undefined = showInlineWarning
+		? { type: 'warning', id: 'invalid-date-field' }
+		: undefined;
+
+	const startDateFieldset = (
+		<FieldSet
+			legend={format === 'rangeInput' ? `${label} start` : label}
+			id={`${id}-start-date`}
+			showLegend={format === 'rangeInput' ? true : !hideLabel}
+			hint={format !== 'rangeInput' ? 'For example: 04 28 1986' : ''}
+			childrenGroupClassName="flex-row flex-align-end usa-memorable-date"
+			className={className}
+			status={format === 'rangeInput' ? undefined : status || inlineWarning}
+		>
+			{Object.keys(inputDetails).map(key => (
+				<div key={key}>
+					<TextInput
+						defaultValue={rangeByVal[key].start}
+						onChange={event => {
+							const newRangeVals = Object.assign({}, rangeByVal);
+							newRangeVals[key].start = event.target.value;
+							setRangeByVal(newRangeVals);
+						}}
+						id={`${id}-${key}-start-date`}
+						{...inputDetails[key].props}
+						{...commonDateInputProps}
+						onBlur={event => {
+							const newRangeVals = Object.assign({}, rangeByVal);
+							newRangeVals[key].startIsInvalid = !inputDetails[key].checkValidity(
+								event.target.value
+							);
+							setRangeByVal(newRangeVals);
+						}}
+						optional={optional}
+						status={
+							rangeByVal[key].startIsInvalid
+								? {
+										type: 'warning',
+										message: inputDetails[key].errorMessage,
+										id: `${id}-${key}-start-error`,
+								  }
+								: undefined
+						}
+					/>
+				</div>
+			))}{' '}
+		</FieldSet>
+	);
+
+	if (format === 'rangeInput') {
+		return (
+			<FieldSet
+				legend={label}
+				status={status || inlineWarning}
+				id={id}
+				showLegend={!hideLabel}
+				hint="For example: 04 28 1986"
+				childrenGroupClassName="flex-row display-flex flex-align-end"
+				className={className}
+			>
+				{startDateFieldset}
+				<FieldSet
+					legend={`${label} end`}
+					id={`${id}-end-date`}
+					showLegend
+					childrenGroupClassName="flex-row flex-align-end usa-memorable-date"
+				>
+					{Object.keys(inputDetails).map(key => (
+						<div key={key}>
+							<TextInput
+								defaultValue={rangeByVal[key].end}
+								onChange={event => {
+									const newRangeVals = Object.assign({}, rangeByVal);
+									newRangeVals[key].end = event.target.value;
+									setRangeByVal(newRangeVals);
+								}}
+								id={`${id}-${key}-end-date`}
+								{...inputDetails[key].props}
+								{...commonDateInputProps}
+								onBlur={event => {
+									const newRangeVals = Object.assign({}, rangeByVal);
+									newRangeVals[key].endIsInvalid = !inputDetails[key].checkValidity(
+										event.target.value
+									);
+									setRangeByVal(newRangeVals);
+								}}
+								optional={optional}
+								status={
+									rangeByVal[key].endIsInvalid
+										? {
+												type: 'warning',
+												message: inputDetails[key].errorMessage,
+												id: `${id}-${key}-start-error`,
+										  }
+										: undefined
+								}
+							/>
+						</div>
+					))}
+				</FieldSet>
+			</FieldSet>
+		);
+	}
+	return startDateFieldset;
+};
