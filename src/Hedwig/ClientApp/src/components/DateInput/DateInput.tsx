590c5816ee8906bd0fdd9c9cc0330859190975a2
Melanie <melanie.mazanec@gmail.com>
2020-02-21 22:23:45 -0500

    Start to convert date input to handle single value rather than range

--- src/Hedwig/ClientApp/src/components/DateInput/DateInput.tsx
+++ src/Hedwig/ClientApp/src/components/DateInput/DateInput.tsx
@@ -1,20 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import moment, { Moment } from 'moment';
-import { DayPickerRangeController, DayPickerSingleDateController } from 'react-dates';
+import { DayPickerSingleDateController } from 'react-dates';
 import { FieldSet, TextInput, FormStatusProps, Button } from '..';
 
-type DateRange = {
-	// TODO: do we want to deal exclusively in range or should we accept just a date too?  Should we make a separate range component?
-	startDate: Moment | null;
-	endDate: Moment | null;
-};
-
 type DateInputProps = {
-	dateRange: DateRange;
-	onChange: (newRange: DateRange) => void;
+	onChange: (newDate: Moment | null) => void;
 	id: string;
 	label: string;
-	format?: 'dayInput' | 'rangeInput';
+	date?: Moment | null;
 	disabled?: boolean;
 	optional?: boolean;
 	status?: FormStatusProps;
@@ -23,25 +16,27 @@ type DateInputProps = {
 	// Will only take effect on fieldsets-- otherwise we should not hide the label
 };
 
+// TODO: MOVE TO UTIL
 const parseDateInput = (input?: string): Moment | null => {
-	if (!input) return null;
-	const acceptedDelimiters = ['-', '/', ' '];
-	acceptedDelimiters.forEach(d => {
-		const splitInput = input.split(d);
-		if (splitInput.length === 3) {
-			// For parsing consistency across browsers
-			return moment(splitInput.join('-'), 'MM-DD-YYYY');
-		}
-	});
-	return null;
+	let parsedInput = null;
+	if (input) {
+		const acceptedDelimiters = ['-', '/', ' '];
+		acceptedDelimiters.forEach(d => {
+			const splitInput = input.split(d);
+			if (splitInput.length === 3) {
+				// For parsing consistency across browsers
+				parsedInput = moment(splitInput.join('-'), 'MM-DD-YYYY');
+			}
+		});
+	}
+	return parsedInput;
 };
 
 export const DateInput: React.FC<DateInputProps> = ({
-	dateRange,
+	date = null,
 	onChange,
 	id,
 	label,
-	format = 'dayInput',
 	disabled,
 	optional,
 	status,
@@ -56,76 +51,59 @@ export const DateInput: React.FC<DateInputProps> = ({
 	// TODO: default to empty date-- what should happen?
 
 	const [calendarOpen, setCalendarOpen] = useState<boolean>(false);
-	const [stringRange, setStringRange] = useState<{ startDate?: string; endDate?: string }>({
-		startDate: dateRange.startDate ? dateRange.startDate.format('MM/DD/YYYY') : undefined,
-		endDate: dateRange.endDate ? dateRange.endDate.format('DD/MM/YYYY') : undefined,
-	});
+	const [stringDate, setStringDate] = useState<string | undefined>(
+		date ? date.format('MM/DD/YYYY') : undefined
+	);
 
 	return (
 		<FieldSet
 			legend={label}
 			id={`${id}-fieldset`}
-			showLegend={format === 'rangeInput'}
 			disabled={disabled}
 			hint="For example: 04/28/1986"
 			status={status}
 			optional={optional}
 			className="oec-date-input oec-date-input-single"
 		>
-			{format === 'dayInput' && (
-				<div className="grid-row flex-row flex-align-end grid-gap position-relative">
-					<TextInput
-						label={label}
-						id={id}
-						onChange={e => setStringRange({ startDate: e.target.value, endDate: e.target.value })}
-						defaultValue={stringRange.startDate}
-						onBlur={() => {
-							const parsedInput = parseDateInput(stringRange.startDate);
-							onChange({ startDate: parsedInput, endDate: parsedInput });
-						}}
+			<div className="grid-row flex-row flex-align-end grid-gap position-relative">
+				<TextInput
+					label={label}
+					id={id}
+					onChange={e => setStringDate(e.target.value)}
+					defaultValue={stringDate}
+					onBlur={() => {
+						const parsedInput = parseDateInput(stringDate);
+						onChange(parsedInput);
+					}}
+				/>
+				<div className="height-auto">
+					{/* TODO: CALENDAR ICON */}
+					<Button
+						text={'C'}
+						onClick={() => setCalendarOpen(!calendarOpen)}
+						aria-label={`${calendarOpen ? 'close' : 'open'} calendar`}
 					/>
-					<div className="height-auto">
-						{/* TODO: CALENDAR ICON */}
-						<Button
-							text={'C'}
-							onClick={() => setCalendarOpen(!calendarOpen)}
-							aria-label={`${calendarOpen ? 'close' : 'open'} calendar`}
+					<div className={`position-absolute z-top ${calendarOpen ? '' : 'display-none'}`}>
+						<DayPickerSingleDateController
+							date={date}
+							onDateChange={newDate => onChange(newDate)}
+							focused={calendarOpen || false}
+							// Annoyingly this does not do anything for keyboard users
+							onFocusChange={f => setCalendarOpen(f.focused || false)}
+							onBlur={() => setCalendarOpen(false)}
+							// TODO: IMPLEMENT ON TAB ONCE TYPES FOR THIS LIBRARY ARE UPDATED :/
+							// onTab={() => {}}
+							onOutsideClick={e => {
+								const targetText = e.target.innerHTML;
+								if (targetText !== 'C') {
+									setCalendarOpen(false);
+								}
+							}}
+							initialVisibleMonth={() => date || moment()}
 						/>
-						{calendarOpen && (
-							<div className="position-absolute z-top">
-								<DayPickerSingleDateController
-									date={dateRange.startDate}
-									onDateChange={newDate => onChange({ startDate: newDate, endDate: newDate })}
-									focused={calendarOpen || false}
-									// Annoyingly this does not do anything for keyboard users
-									onFocusChange={f => setCalendarOpen(f.focused || false)}
-									onOutsideClick={e => {
-										const targetText = e.target.innerHTML;
-										if (targetText !== 'C') {
-											setCalendarOpen(false);
-										}
-									}}
-								/>
-							</div>
-						)}
 					</div>
 				</div>
-			)}
-			{/* {format === 'rangeInput' && (
-				// TODO: if line must break then arrow is on first line and calendar is on second
-				<>
-					<TextInput label="Start date" id={`${id}-start`} />
-					<span>TODO: PUT AN ARROW HERE</span>
-					<TextInput label="End date" id={`${id}-end`} />
-					<DayPickerRangeController
-						startDate={dateRange.startDate}
-						endDate={dateRange.endDate}
-						onDatesChange={onChange}
-						focusedInput={calendarOpen}
-						onFocusChange={setCalendarOpen}
-					/>
-				</>
-			)} */}
+			</div>
 		</FieldSet>
 	);
 };
