b2da9fb600717f350e1173615f18917eb232cbba
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2020-02-25 17:49:43 -0500

    Prevent withdraw for missing info (PR #795) (Issue #556)
    * Prevent withdraw for missing info
    
    * Use alert text maker
    
    * Format

--- src/Hedwig/ClientApp/src/containers/Withdrawal/Withdrawal.tsx
+++ src/Hedwig/ClientApp/src/containers/Withdrawal/Withdrawal.tsx
@@ -29,10 +29,12 @@ import {
 	clientErrorForField,
 	serverErrorForField,
 	useFocusFirstError,
+	hasValidationErrors,
 } from '../../utils/validations';
 import ReportingPeriodContext from '../../contexts/ReportingPeriod/ReportingPeriodContext';
 import { processBlockingValidationErrors } from '../../utils/validations/processBlockingValidationErrors';
 import AlertContext from '../../contexts/Alert/AlertContext';
+import { missingInformationForWithdrawalAlert } from '../../utils/stringFormatters/alertTextMakers';
 
 type WithdrawalProps = {
 	history: History;
@@ -89,26 +91,21 @@ export default function Withdrawal({
 	const cdcFunding = currentCdcFunding(cdcFundings);
 
 	useEffect(() => {
-		updateReportingPeriodOptions(
-			lastNReportingPeriods(reportingPeriods, enrollmentEndDate || moment().toDate(), 5)
-		);
+		if (reportingPeriods) {
+			updateReportingPeriodOptions(
+				lastNReportingPeriods(reportingPeriods, enrollmentEndDate || moment().toDate(), 5)
+			);
+		}
 	}, [reportingPeriods, enrollmentEndDate]);
 
-	// TODO: replace this whole endeavor with ability to simply select a first reporting period in this form
-	const cannotWithdraw = cdcFunding && !cdcFunding.firstReportingPeriod;
+	const isMissingInformation = hasValidationErrors(enrollment);
+
 	useEffect(() => {
-		if (cannotWithdraw) {
-			setAlerts([
-				{
-					type: 'error',
-					heading: 'Information needed to withdraw child',
-					text:
-						'To withdraw a child from a funded space in your program, "first reporting period" cannot be blank. Please update this information for the child before withdrawing',
-				},
-			]);
+		if (isMissingInformation) {
+			setAlerts([missingInformationForWithdrawalAlert]);
 			history.push(`/roster/sites/${siteId}/enrollments/${enrollment.id}`);
 		}
-	}, [enrollment, cannotWithdraw, history, setAlerts]);
+	}, [enrollment, isMissingInformation, history, setAlerts]);
 
 	useFocusFirstError([apiError]);
 
@@ -259,7 +256,7 @@ export default function Withdrawal({
 							status={serverErrorForField(
 								'fundings',
 								apiError,
-								lastReportingPeriod ? undefined : 'This information is required for withdrawal'
+								'This information is required for withdrawal'
 							)}
 						/>
 					)}
@@ -274,7 +271,7 @@ export default function Withdrawal({
 						/>
 					</div>
 					<div className="mobile-lg:grid-col-auto padding-left-2">
-						<Button text="Confirm and withdraw" onClick={save} disabled={cannotWithdraw} />
+						<Button text="Confirm and withdraw" onClick={save} disabled={isMissingInformation} />
 					</div>
 				</div>
 			</div>
