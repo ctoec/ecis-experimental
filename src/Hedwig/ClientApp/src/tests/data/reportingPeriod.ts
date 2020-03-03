d2539450f40e72388e75ba05f45061dfdfb3b290
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2020-03-03 14:41:30 -0500

    Sonar cloud smh

--- src/Hedwig/ClientApp/src/tests/data/reportingPeriod.ts
+++ src/Hedwig/ClientApp/src/tests/data/reportingPeriod.ts
@@ -1,28 +1,39 @@
 import { ReportingPeriod, FundingSource } from '../../generated';
 
+const defaultCdcReportingPeriod = { type: FundingSource.CDC };
+
+const marchFirst = new Date('2019-03-01');
+const marchReportingPeriod = {
+	...defaultCdcReportingPeriod,
+	id: 1,
+	period: marchFirst,
+	periodStart: marchFirst,
+	periodEnd: new Date('2019-03-31'),
+	dueAt: new Date('2019-04-15'),
+};
+
+const aprilFirst = new Date('2019-04-01');
+const aprilReportingPeriod = {
+	...defaultCdcReportingPeriod,
+	id: 2,
+	period: aprilFirst,
+	periodStart: aprilFirst,
+	periodEnd: new Date('2019-04-30'),
+	dueAt: new Date('2019-05-15'),
+};
+
+const mayFirst = new Date('2019-05-01');
+const mayReportingPeriod = {
+	...defaultCdcReportingPeriod,
+	id: 3,
+	period: mayFirst,
+	periodStart: mayFirst,
+	periodEnd: new Date('2019-05-31'),
+	dueAt: new Date('2019-06-15'),
+};
+
 export const cdcReportingPeriods: ReportingPeriod[] = [
-	{
-		id: 1,
-		type: FundingSource.CDC,
-		period: new Date('2019-03-01'),
-		periodStart: new Date('2019-03-01'),
-		periodEnd: new Date('2019-03-31'),
-		dueAt: new Date('2019-04-15'),
-	},
-	{
-		id: 2,
-		type: FundingSource.CDC,
-		period: new Date('2019-04-01'),
-		periodStart: new Date('2019-04-01'),
-		periodEnd: new Date('2019-04-30'),
-		dueAt: new Date('2019-05-15'),
-	},
-	{
-		id: 3,
-		type: FundingSource.CDC,
-		period: new Date('2019-05-01'),
-		periodStart: new Date('2019-05-01'),
-		periodEnd: new Date('2019-05-31'),
-		dueAt: new Date('2019-06-15'),
-	},
+	marchReportingPeriod,
+	aprilReportingPeriod,
+	mayReportingPeriod,
 ];
