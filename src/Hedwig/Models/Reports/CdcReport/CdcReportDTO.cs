6de0fa77b8e1fc522bb931281429f13230392500
julia <julia@skylight.digital>
2020-04-23 15:01:11 -0400

    Remove unnecessary fields from report dtos

--- src/Hedwig/Models/Reports/CdcReport/CdcReportDTO.cs
+++ src/Hedwig/Models/Reports/CdcReport/CdcReportDTO.cs
@@ -7,7 +7,6 @@ namespace Hedwig.Models
 	public class CdcReportDTO
 	{
 		public int Id { get; set; }
-		public FundingSource Type { get; set; }
 		public ReportingPeriod ReportingPeriod { get; set; }
 		public DateTime? SubmittedAt { get; set; }
 		public CdcReportOrganizationDTO Organization { get; set; }
--- src/Hedwig/Models/Reports/OrganizationReport/OrganizationReportSummaryDTO.cs
+++ src/Hedwig/Models/Reports/OrganizationReport/OrganizationReportSummaryDTO.cs
@@ -5,7 +5,8 @@ namespace Hedwig.Models
 	public class OrganizationReportSummaryDTO
 	{
 		public int Id { get; set; }
-		public FundingSource Type { get; set; }
+		// For now, the ReportsController only returns CDC reports
+		// public FundingSource Type { get; set; }
 		public ReportingPeriod ReportingPeriod { get; set; }
 		public DateTime? SubmittedAt { get; set; }
 		public OrganizationReportSummaryOrganizationDTO Organization { get; set; }
