73976adf9f4ae02830758b507726011601711d7a
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2020-04-24 14:30:44 -0400

    PR comments

--- test/HedwigTests/Fixtures/TestStartup.cs
+++ test/HedwigTests/Fixtures/TestStartup.cs
@@ -28,37 +28,7 @@ namespace HedwigTests.Fixtures
 			services.ConfigureAuthorization();
 			services.ConfigureValidation();
 			services.AddSingleton<IDateTime, SystemDateTime>();
-			services.AddSwaggerGen(c =>
-			{
-				c.SwaggerDoc("v1", new OpenApiInfo { Title = "Hedwig API", Version = "v1" });
-				c.DescribeAllEnumsAsStrings();
-				c.TagActionsBy(api => new List<string> { "Hedwig" });
-				c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-				{
-					Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
-					Name = "Authorization",
-					In = ParameterLocation.Header,
-					Type = SecuritySchemeType.ApiKey,
-					Scheme = "Bearer"
-				});
-				c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-				{
-					{
-						new OpenApiSecurityScheme
-						{
-							Reference = new OpenApiReference
-							{
-								Type = ReferenceType.SecurityScheme,
-								Id = "Bearer"
-							},
-							Scheme = "oauth2",
-							Name = "Bearer",
-							In = ParameterLocation.Header
-						},
-						new List<string> { }
-					}
-				});
-			});
+			services.ConfigureSwagger();
 			services.AddHttpContextAccessor();
 
 			// Test Startup
