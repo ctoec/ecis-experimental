c0a4a18ec11a5d67d61548b85c54ab8267b1afa6
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2020-04-24 14:14:11 -0400

    Fix flaky hosted service test

--- test/HedwigTests/Fixtures/TestStartup.cs
+++ test/HedwigTests/Fixtures/TestStartup.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using Hedwig;
+using Hedwig.Utilities;
 
 namespace HedwigTests.Fixtures
 {
@@ -11,9 +14,54 @@ namespace HedwigTests.Fixtures
 
 		public override void ConfigureServices(IServiceCollection services)
 		{
-			base.ConfigureServices(services);
+			// Test Startup
 			services.ConfigureSqlServer(Configuration.GetConnectionString("HEDWIG"));
+			// Main Startup
+			// Excluding ConfigureSqlServer and ConfigureHostedServices
+			services.ConfigureCors();
+			services.ConfigureFilters();
+			services.ConfigureControllers();
+			services.ConfigureSpa();
+			services.ConfigureMapping();
+			services.ConfigureRepositories();
+			services.ConfigureAuthentication(Configuration.GetValue<string>("WingedKeysUri"));
+			services.ConfigureAuthorization();
+			services.ConfigureValidation();
+			services.AddSingleton<IDateTime, SystemDateTime>();
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new OpenApiInfo { Title = "Hedwig API", Version = "v1" });
+				c.DescribeAllEnumsAsStrings();
+				c.TagActionsBy(api => new List<string> { "Hedwig" });
+				c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+				{
+					Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+					Name = "Authorization",
+					In = ParameterLocation.Header,
+					Type = SecuritySchemeType.ApiKey,
+					Scheme = "Bearer"
+				});
+				c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+				{
+					{
+						new OpenApiSecurityScheme
+						{
+							Reference = new OpenApiReference
+							{
+								Type = ReferenceType.SecurityScheme,
+								Id = "Bearer"
+							},
+							Scheme = "oauth2",
+							Name = "Bearer",
+							In = ParameterLocation.Header
+						},
+						new List<string> { }
+					}
+				});
+			});
+			services.AddHttpContextAccessor();
 
+			// Test Startup
 			if (TestEnvironmentFlags.ShouldLogSQL())
 			{
 				services.AddLogging(configure => configure.AddConsole());
