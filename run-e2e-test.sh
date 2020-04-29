696545f0e56c1df93cda473719d9e462a1d4506e
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2020-04-29 14:50:36 -0400

    Restructure e2e docker compose

---
+++ run-e2e-test.sh
@@ -0,0 +1,8 @@
+echo "Waiting for backend server to be running..."
+/wait-for-it.sh -t 120 --host=backend --port=5001
+if [ "$?" -eq "0" ]; then
+	echo "Beginning End-to-End tests..."
+	yarn test:e2e:browserstack
+else
+	echo "Server was not available within timeout window"
+fi
