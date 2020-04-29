f2ceec46ba6da48dc3766d561a84dab2482d9b0a
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2020-04-29 13:21:54 -0400

    Update docker to use wait for

--- entrypoint-e2e-test.sh
+++ entrypoint-e2e-test.sh
@@ -3,6 +3,10 @@ if [ -z "$nm" ]; then
     echo "installing yarn dependencies..."
     yarn install
 fi
-echo "starting client..."
-yarn test:e2e:browserstack
-exit 0
+/wait-for-it.sh -t 120 --host=backend-for-selenium --port=5001
+if [ "$?" -eq "0" ]; then
+	echo "Beginning End-to-End tests..."
+	yarn test:e2e:browserstack
+else
+	echo "Server was not available within timeout window"
+fi
