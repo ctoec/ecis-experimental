ede2f3b09f56748adc7e1ac91e4db7280075fc15
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2020-05-11 09:34:51 -0400

    Fix address formatter

--- src/Hedwig/ClientApp/src/utils/models/family.ts
+++ src/Hedwig/ClientApp/src/utils/models/family.ts
@@ -19,13 +19,17 @@ export function addressFormatter(family?: Family) {
 	if (!family) {
 		return [''];
 	}
-	var street = family.addressLine1 ? family.addressLine1 : '';
+	var addressLine1 = family.addressLine1 ? family.addressLine1 : '';
+	var addressLine2 = family.addressLine2 ? ` ${family.addressLine2}` : '';
 	var town = family.town ? family.town : '';
 	var state = family.state ? family.state : '';
 	var zip = family.zip ? family.zip : '';
 
-	var fullAddress = !street && !town && !state && !zip ? '' : `${street}, ${town}, ${state} ${zip}`;
-	if (!street || !town || !state || !zip) {
+	var fullAddress =
+		!addressLine1 && !town && !state && !zip
+			? ''
+			: `${addressLine1}${addressLine2}, ${town}, ${state} ${zip}`;
+	if (!addressLine1 || !town || !state || !zip) {
 		return [fullAddress, InlineIcon({ icon: 'incomplete' })];
 	}
 
