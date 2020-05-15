8ef1519204e653469538805a3da90228b85a087b
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2020-05-15 10:21:21 -0400

    Guard UsaSpa and add default handler

--- src/Hedwig/Startup.cs
+++ src/Hedwig/Startup.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -79,52 +81,64 @@ namespace Hedwig
 				// Connected to note on services.ConfigureSwagger()
 				app.UseSwagger();
 			}
+
 			app.UseRouting();
 
 			app.UseAuthentication();
 			app.UseAuthorization();
-
-			// Use SPA static files in all non-dev environments
-			if (!env.IsDevelopment())
-			{
-				app.UseSpaStaticFiles();
-			}
 
 			app.UseEndpoints(endpoints =>
 			{
 				endpoints.MapControllers();
 			});
 
-			app.UseSpa(spa =>
-			{
-				spa.Options.SourcePath = "ClientApp";
-
-				// If we are development, that means we aren't serving SPA files
-				// as static, compiled resources. So we need to forward requests a
-				// development server.
-				if (env.IsDevelopment())
+			// UseSpa internally calls UseStaticFiles which only supports GET or OPTIONS
+			// requests. Guard this middleware so only requests with those methods are 
+			// able to reach this. This prevents a 500 error when a POST/HEAD/DELETE/PUT
+			// reaches this middleware.
+			app.UseWhen(
+				context => HttpMethods.IsGet(context.Request.Method) || HttpMethods.IsOptions(context.Request.Method),
+				app => app.UseSpa(spa =>
 				{
-					var isDocker = Environment.GetEnvironmentVariable("DOCKER_DEVELOPMENT");
-					// If we are using docker (compose), the client container will serve
-					// responses for us. We need to forward to its hostname.
-					if (isDocker == "true")
-					{
-						string CLIENT_HOST = Environment.GetEnvironmentVariable("CLIENT_HOST") ?? "http://localhost:3000";
-						spa.UseProxyToSpaDevelopmentServer(CLIENT_HOST);
-					}
-					// If we aren't using docker, we need .NET to create a server for us
-					// to serve responses.
-					else
-					{
-						// Note: While the parameter is called npmScript it will also run yarn
-						// scripts. Specifically, it will run the corresponding package.json 
-						// script and provided the referenced executable exists on the path
-						// the script will run.
-						spa.UseReactDevelopmentServer(npmScript: "start");
-					}
-				}
-			});
+					spa.Options.SourcePath = "ClientApp";
 
+					// If we are development, that means we aren't serving SPA files
+					// as static, compiled resources. So we need to forward requests a
+					// development server.
+					if (env.IsDevelopment())
+					{
+						var isDocker = Environment.GetEnvironmentVariable("DOCKER_DEVELOPMENT");
+						// If we are using docker (compose), the client container will serve
+						// responses for us. We need to forward to its hostname.
+						if (isDocker == "true")
+						{
+							string CLIENT_HOST = Environment.GetEnvironmentVariable("CLIENT_HOST") ?? "http://localhost:3000";
+							spa.UseProxyToSpaDevelopmentServer(CLIENT_HOST);
+						}
+						// If we aren't using docker, we need .NET to create a server for us
+						// to serve responses.
+						else
+						{
+							// Note: While the parameter is called npmScript it will also run yarn
+							// scripts. Specifically, it will run the corresponding package.json 
+							// script and provided the referenced executable exists on the path
+							// the script will run.
+							spa.UseReactDevelopmentServer(npmScript: "start");
+						}
+					}
+
+					// If we aren't in development, UseSpa will internally call UseStaticFiles
+					// on the directory specified by AddSpaStaticFiles
+				})
+			);
+
+			// Catch any remaining requests and return 404 not found
+			app.Run(
+				context =>
+				{
+					context.Response.StatusCode = StatusCodes.Status404NotFound;
+					return Task.CompletedTask;
+				});
 		}
 	}
 }
