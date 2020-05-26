338e8301220500784cfb610c6151a418c06df19f
julia <julia@skylight.digital>
2020-05-26 11:26:36 -0400

    WIP

---
+++ src/Hedwig/ClientApp/src/containers/Enrollment/_sections/EnrollmentFunding/Fields/AgeGroup.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { RadioButtonGroupProps, RadioButtonGroup } from "../../../../../components/RadioButtonGroup/RadioButtonGroup"
+import { Age, Enrollment } from '../../../../../generated';
+import FormField from '../../../../../components/Form_New/FormField';
+import { ageFromString, prettyAge } from '../../../../../utils/models';
+import RadioButton from '../../../../../components/RadioButton/RadioButton';
+import { initialLoadErrorGuard } from '../../../../../utils/validations';
+import { displayValidationStatus } from '../../../../../utils/validations/displayValidationStatus';
+import { REQUIRED_FOR_OEC_REPORTING } from '../../../../../utils/validations/messageStrings';
+
+export const AgeGroupField: React.FC<{initalLoad: boolean}> = ({ initialLoad }) => {
+	return (
+		<FormField<Enrollment, RadioButtonGroupProps, Age | null>
+			getValue={data => data.at('ageGroup')}
+			parseOnChangeEvent={e => ageFromString((e.target as HTMLInputElement).value)}
+			inputComponent={RadioButtonGroup}
+			id="age-group-radiogroup"
+			legend="Age group"
+			options={[
+				{
+					render: (props) => <RadioButton text={prettyAge(Age.InfantToddler)} {...props} />,
+					value: Age.InfantToddler,
+				},
+				{
+					render: (props) => <RadioButton text={prettyAge(Age.Preschool)} {...props} />,
+					value: Age.SchoolAge,
+				},
+				{
+					render: (props) => <RadioButton text={prettyAge(Age.SchoolAge)} {...props} />,
+					value: Age.SchoolAge,
+				}
+			]}
+			status={data => 
+				initialLoadErrorGuard(
+					initialLoad,
+					displayValidationStatus([
+						{
+							type: 'warning',
+							response: data.at('validationErrors').value,
+							field: 'ageGroup',
+							message: REQUIRED_FOR_OEC_REPORTING
+						}
+					])
+				)
+			}
+		/>
+	)
+}
---
+++ src/Hedwig/ClientApp/src/containers/Enrollment/_sections/EnrollmentFunding/Fields/StartDate.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { DateInputProps, DateInput } from "../../../../../components"
+import FormField from '../../../../../components/Form_New/FormField';
+import { Enrollment } from '../../../../../generated';
+import { displayValidationStatus } from '../../../../../utils/validations/displayValidationStatus';
+import { initialLoadErrorGuard } from '../../../../../utils/validations';
+
+export const StartDateField: React.FC<{ initialLoad: boolean}> = ({ initialLoad }) => {
+	return (
+		<div>
+			<FormField<Enrollment, DateInputProps, Date | null>
+				getValue={data => data.at('entry')}
+				parseOnChangeEvent={e => (e as any).toDate()}
+				inputComponent={DateInput}
+				status={data => 
+					initialLoadErrorGuard(
+						initialLoad,
+						displayValidationStatus([
+							{
+								type: 'warning',
+								response: data.at('validationErrors').value,
+								field: 'entry'
+							}
+						])
+					)
+				}
+				label="Start date"
+				id="start-date"
+			/>
+		</div>
+	)
+}
