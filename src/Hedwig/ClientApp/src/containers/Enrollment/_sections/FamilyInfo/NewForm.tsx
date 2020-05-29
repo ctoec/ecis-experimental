ee39dba56de37191697078378f2c8341b23a44d0
Melanie <melanie.mazanec@gmail.com>
2020-05-29 16:05:39 -0400

    Refactor new form

--- src/Hedwig/ClientApp/src/containers/Enrollment/_sections/FamilyInfo/NewForm.tsx
+++ src/Hedwig/ClientApp/src/containers/Enrollment/_sections/FamilyInfo/NewForm.tsx
@@ -1,10 +1,9 @@
 import FamilyInfo from '.';
-import React, { useState, useReducer, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import useApi, { ApiError } from '../../../../hooks/useApi';
 import { useFocusFirstError } from '../../../../utils/validations';
 import useCatchallErrorAlert from '../../../../hooks/useCatchallErrorAlert';
-import { FormReducer, formReducer, updateData } from '../../../../utils/forms/form';
-import { DeepNonUndefineable } from '../../../../utils/types';
+import Form from '../../../../components/Form_New/Form';
 import {
 	Enrollment,
 	ApiOrganizationsOrgIdSitesSiteIdEnrollmentsIdPutRequest,
@@ -18,7 +17,6 @@ import {
 import FormSubmitButton from '../../../../components/Form_New/FormSubmitButton';
 import { Address, FosterCheckbox, HomelessnessCheckbox } from './Fields';
 import { SectionProps } from '../../enrollmentTypes';
-import Form from '../../../../components/Form_New/Form';
 
 export const NewForm: React.FC<SectionProps> = ({
 	enrollment,
@@ -27,82 +25,90 @@ export const NewForm: React.FC<SectionProps> = ({
 	successCallback,
 	onSectionTouch,
 	touchedSections,
-}: any) => {
+}) => {
 	if (!enrollment || !enrollment.child) {
 		throw new Error('FamilyInfo rendered without a child');
 	}
 
-	// set up form state
-	const initialLoad = touchedSections ? !touchedSections[FamilyInfo.key] : false;
-	const [error, setError] = useState<ApiError | null>(inputError);
-
-	useFocusFirstError([error]);
-	useCatchallErrorAlert(error);
-
-	const [_enrollment, updateEnrollment] = useReducer<FormReducer<DeepNonUndefineable<Enrollment>>>(
-		formReducer,
-		enrollment
-	);
-	const updateFormData = updateData<DeepNonUndefineable<Enrollment>>(updateEnrollment);
-
+	// Can we move this duplicated logic up to a wrapper component or something?
 	const { user } = useContext(UserContext);
 
-	if (!_enrollment.child.family) {
-		// If there isn't a family, create one-- otherwise user can save section without making one
-		updateFormData(e => e.value)({
-			name: 'child.family',
-			value: createEmptyFamily(getIdForUser(user, 'org'), enrollment.child.familyId || 0),
-		});
+	const [attemptSave, setAttemptSave] = useState(false);
+
+	const initialEnrollment = {
+		...{
+			child: {
+				family: createEmptyFamily(getIdForUser(user, 'org'), enrollment.child.familyId || 0),
+			}
+		},
+		...enrollment
 	}
-	const child = _enrollment.child;
-	const { foster, family } = child;
-	const { addressLine1, addressLine2, town, state, zip, homelessness } = family || {};
+
+	const [mutatedEnrollment, setMutatedEnrollment] = useState<Enrollment>(initialEnrollment as Enrollment)
 
 	const defaultParams: ApiOrganizationsOrgIdSitesSiteIdEnrollmentsIdPutRequest = {
-		id: _enrollment.id,
+		id: mutatedEnrollment.id,
 		orgId: getIdForUser(user, 'org'),
 		siteId: validatePermissions(user, 'site', siteId) ? siteId : 0,
-		enrollment: _enrollment,
+		enrollment: mutatedEnrollment,
 	};
 
-	const [attemptingSave, setAttemptingSave] = useState(false);
-	const { error: saveError, data: saveData } = useApi<Enrollment>(
+	const { error: errorOnSave, data: returnedEnrollment, loading: isSaving } = useApi<Enrollment>(
 		api => api.apiOrganizationsOrgIdSitesSiteIdEnrollmentsIdPut(defaultParams),
 		{
-			skip: !attemptingSave,
+			skip: !attemptSave,
 			callback: () => {
-				setAttemptingSave(false);
+				setAttemptSave(false);
 				onSectionTouch && onSectionTouch(FamilyInfo);
 			},
 		}
 	);
 
+	// do we need this??
+	const initialLoad = touchedSections ? !touchedSections[FamilyInfo.key] : false;
+
+	// Set to input error iniitally, update with error after put is fired
+	const [error, setError] = useState<ApiError | null>(inputError);
+	useFocusFirstError([error]);
+	useCatchallErrorAlert(error);
+
+	const child = mutatedEnrollment.child;
+	const { foster, family } = child || {};
+	const { addressLine1, addressLine2, town, state, zip, homelessness } = family || {};
+
 	useEffect(() => {
 		// If the request went through, then do the next steps
-		if (!saveData && !saveError) {
+		if (!returnedEnrollment && !errorOnSave) {
 			return;
 		}
 		// Set the new error regardless of whether there is one
-		setError(saveError);
-		if (saveData && !saveError) {
-			if (successCallback) successCallback(saveData);
+		setError(errorOnSave);
+		if (returnedEnrollment && !errorOnSave) {
+			if (successCallback) successCallback(returnedEnrollment);
 		}
-	}, [saveData, saveError]);
+	}, [returnedEnrollment, errorOnSave]);
+
+	const onFormSubmit = (userModifiedEnrollment: Enrollment) => {
+		// Kick off the API request
+		setAttemptSave(true);
+		// Apply the mutations to the local copy of the enrollment
+		setMutatedEnrollment(userModifiedEnrollment);
+	};
 
 	return (
-		<Form
-			data={_enrollment}
+		<Form<Enrollment>
 			className="FamilyInfoForm usa-form"
+			data={mutatedEnrollment}
 			noValidate
 			autoComplete="off"
-			onSubmit={() => setAttemptingSave(true)}
+			onSubmit={onFormSubmit}
 		>
 			<h2>Address</h2>
-			<Address {...{ initialLoad, child, addressLine1, addressLine2, zip, state, updateFormData, town }} />
+			<Address {...{ initialLoad, child, addressLine1, addressLine2, zip, state, town }} />
 			<h2>Other</h2>
-			<FosterCheckbox foster={foster} updateFormData={updateFormData} />
-			<HomelessnessCheckbox homelessness={homelessness} updateFormData={updateFormData} />
-			<FormSubmitButton text={attemptingSave ? 'Saving...' : 'Save'} disabled={attemptingSave} />
+			<FosterCheckbox foster={foster} />
+			<HomelessnessCheckbox homelessness={homelessness} />
+			<FormSubmitButton text={isSaving ? 'Saving...' : 'Save'} disabled={isSaving} />
 		</Form>
 	);
 };
