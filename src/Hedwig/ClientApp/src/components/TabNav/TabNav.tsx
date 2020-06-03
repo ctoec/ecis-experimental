20b9245fcb7664d97af8ca8922aedd841187c163
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2020-06-03 16:03:45 -0400

    Add tab nav with example

---
+++ src/Hedwig/ClientApp/src/components/TabNav/TabNav.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import cx from 'classnames';
+
+type TabItemProps = {
+	text: JSX.Element | string;
+	content: JSX.Element;
+	default?: true;
+};
+
+type TabNavProps = {
+	items: TabItemProps[];
+};
+
+export const TabNav: React.FC<TabNavProps> = ({ items }) => {
+	const indexOfDefault = items.findIndex((item) => item.default);
+	const defaultActiveIndex = indexOfDefault < 0 ? 0 : indexOfDefault;
+	const [activeTabIndex, setActiveTabIndex] = useState(defaultActiveIndex);
+
+	const onClick = (index: number) => {
+		setActiveTabIndex(index);
+	};
+
+	const tabs = items.map(({ text }, index) => (
+		<div
+			key={index}
+			className={cx('oec-tab-nav--tab', { 'oec-tab-nav--tab__active': index === activeTabIndex })}
+			onClick={() => onClick(index)}
+		>
+			{text}
+		</div>
+	));
+
+	const activeTab = items[activeTabIndex];
+
+	return (
+		<div className="oec-tab-nav">
+			<div className="oec-tab-nav--header">{tabs}</div>
+			<div className="oec-tab-nav--divider"></div>
+			<div className="oec-tab-nav--content">{activeTab.content}</div>
+		</div>
+	);
+};
