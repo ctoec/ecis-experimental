fc960d06751878b8b829377533d46eed87b8b79b
julia <julia@skylight.digital>
2020-06-04 00:46:11 -0400

    Storyshot tests have fixed mock date

--- src/Hedwig/ClientApp/src/components/DateInput/DateInput.stories.tsx
+++ src/Hedwig/ClientApp/src/components/DateInput/DateInput.stories.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import moment from 'moment';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { DateInput, FormStatusProps } from '..';
