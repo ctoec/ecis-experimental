32968ec12c3be449914a9acb2304619dddd99454
Melanie <melanie.mazanec@gmail.com>
2020-06-10 16:51:37 -0400

    Start to add certs

--- src/Hedwig/ClientApp/src/containers/Enrollment/_sections/EnrollmentFunding_NEW/UpdateForm/Care4Kids/index.tsx
+++ src/Hedwig/ClientApp/src/containers/Enrollment/_sections/EnrollmentFunding_NEW/UpdateForm/Care4Kids/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { SectionProps } from '../../../../enrollmentTypes';
 import { UpdateFormSectionProps } from '../common';
+import { getCurrentC4kCertificate } from '../../../../../../utils/models';
+import { C4KCertificateCard } from './C4KCertificateCard';
+import { C4KCertificate } from '../../../../../../generated';
 
 export const Care4KidsForm: React.FC<UpdateFormSectionProps> = ({
 	mutatedEnrollment,
@@ -12,5 +14,15 @@ export const Care4KidsForm: React.FC<UpdateFormSectionProps> = ({
 		throw new Error('Section rendered without enrollment or child');
 	}
 
-	return <>CARE FOR KIDS</>;
+	// get current cert
+	const c4kFunding = getCurrentC4kCertificate(mutatedEnrollment);
+	// get past certs
+	const pastC4kCerts: C4KCertificate[] = []
+
+	return <>
+		{c4kFunding && <><h2>Current certificate</h2>
+			<C4KCertificateCard certificate={c4kFunding} c4KFamilyId={c4kFunding.child?.familyId || 0} isCurrent /></>}
+		{pastC4kCerts.length > 0 &&
+			<><h2>Past Care 4 Kids certificates</h2>{pastC4kCerts.map(pastCert => <C4KCertificateCard certificate={pastCert} c4KFamilyId={} />)}</>}
+	</>;
 };
