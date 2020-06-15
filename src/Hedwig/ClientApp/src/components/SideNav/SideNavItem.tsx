31d08de0f8e1d927605f4f9ee0ecb7428652dbd7
julia <julia@skylight.digital>
2020-06-15 15:57:31 -0400

    947: Update sidenav to function more like tabnav

--- src/Hedwig/ClientApp/src/components/SideNav/SideNavItem.tsx
+++ src/Hedwig/ClientApp/src/components/SideNav/SideNavItem.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import cx from 'classnames';
 import { InlineIcon, Icon } from '..';
+import { TextWithIcon } from '../TextWithIcon/TextWithIcon';
 
 type SideNavItemTitle = {
 	text: string;
@@ -10,9 +11,14 @@ type SideNavItemTitle = {
 export type SideNavItemProps = {
 	titleLink: SideNavItemTitle;
 	description: string;
-	active?: boolean;
 	icon?: Icon;
 	onClick?: () => any;
+	content: JSX.Element;
+};
+
+type InternalSideNavItemProps = SideNavItemProps & {
+	active: boolean;
+	onClick: () => any;
 };
 
 export const SideNavItem = ({
@@ -21,15 +27,14 @@ export const SideNavItem = ({
 	active,
 	onClick,
 	icon,
-}: SideNavItemProps) => {
+}: InternalSideNavItemProps) => {
 	return (
 		<li className={cx('oec-sidenav__item', { active })}>
-			<a href={titleLink.link} onClick={onClick}>
-				<div className="oec-sidenav-item__title">
-					{titleLink.text}
-					{icon && <InlineIcon icon={icon} />}
+			<a onClick={onClick}>
+				<div>
+					<p className="oec-sidenav-item__title">{titleLink.text} {icon && <InlineIcon icon={icon} /> }</p>
+					<p className="oec-sidenav-item__desc">{description}</p>
 				</div>
-				<div className="oec-sidenav-item__desc">{description}</div>
 			</a>
 		</li>
 	);
