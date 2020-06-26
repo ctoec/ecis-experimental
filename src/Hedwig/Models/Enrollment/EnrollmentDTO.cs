95646a3bb66b25388e6a83d81b6166fc16734985
Melanie <melanie.mazanec@gmail.com>
2020-06-26 11:28:32 -0400

    DItch cruft

--- src/Hedwig/Models/Enrollment/EnrollmentDTO.cs
+++ src/Hedwig/Models/Enrollment/EnrollmentDTO.cs
@@ -16,8 +16,6 @@ namespace Hedwig.Models
 		public DateTime? Exit { get; set; }
 		public string ExitReason { get; set; }
 		public List<FundingDTO> Fundings { get; set; }
-		// TODO: maybe remove summary DTO?
-		public List<EnrollmentSummaryDTO> PastEnrollments { get; set; }
 		public List<ValidationError> ValidationErrors { get; set; }
 	}
 }
