e19b242fe770b43f06335b8ebb0a4153084f8cca
Jordan Guinn <jordan@skylight.digital>
2020-09-08 09:23:07 -0700

    Set up Hedwig deploys across environments in CircleCI (#1275)
    Hedwig now has CI/CD within CircleCI! This PR handles a number of different things:
    
    - Creates a CI/CD environment for Hedwig in CircleCI
    - Deploys all merges to master directly to our staging environment
    - Provides the option to deploys all commits on all branches to any environment
    - Removes the Terraform infrastructure for Hedwig's previous PR system (which auto-generated infrastructure)
    - Codifies the manually written release pipeline previously in Azure DevOps
    - Removes all the IIS-specific installation configuration (as Hedwig is now running on Linux w/.NET Core, not Windows Server)
    - Configures NGINX (the reverse proxy in front of our Linux server) to redirect all HTTP traffic to HTTPS

--- installer/tf/prsystem/terraform.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "ct-oec-hedwig-cicd-pr-terraform"
-    key            = "terraform.tfstate"
-    region         = "us-east-2"
-    encrypt        = true
-  }
-}
--- installer/tf/prsystem/tf-init.bat
+++
@@ -1,1 +0,0 @@
-terraform init -input=false -lock=true -lock-timeout=0s -force-copy -backend=true -backend-config="bucket=ct-oec-hedwig-cicd-pr-terraform" -backend-config="key=terraform.tfstate" -backend-config="region=us-east-2" -get=true -get-plugins=true  -verify-plugins=true 
